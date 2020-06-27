b3e33e98010c4e4041fa817aae91cd059da090f4
bmm00000 <bmm00000@icloud.com>
2020-06-27 19:54:24 +0200

    async js

--- js.js
+++ js.js
@@ -137,3 +137,235 @@ const annoyer = {
 		clearInterval(this.timerID);
 	}
 };
+
+function multiply(x, y) {
+	y = typeof y === 'undefined' ? 1 : y;
+	return x * y;
+}
+
+const greeting = (person, greeting = 'hi') => {
+	console.log(`${greeting}${person}!!!`);
+};
+
+function sum() {
+	const arg = [ ...arguments ];
+	return arg.reduce((total, currVal) => {
+		return total + currVal;
+	});
+}
+
+function sum(...nums) {
+	const total = 0;
+	for (let num of nums) {
+		total += num;
+	}
+	return total;
+}
+
+function win([ first ]) {
+	console.log(`${first} is the best!`);
+}
+
+const winner = [ 'jose', 'pablo', 'laura' ];
+
+win(winner);
+
+const annoyer = {
+	phrases: [ 'bonito', 'bueno', 'barato' ],
+	pickPhrase() {
+		const index = Math.floor(Math.random() * this.phrases.length);
+		console.log(this.phrases[index]);
+	},
+	start() {
+		this.intervalId = setInterval(() => {
+			this.pickPhrase();
+		}, 500);
+	},
+	stop() {
+		clearInterval(this.intervalId);
+	}
+};
+
+annoyer.start();
+
+function makeDeck() {
+	const deck = [];
+	const suits = [ 'Hearts', 'Diamonds', 'Spades', 'Clubs' ];
+	const values = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'A', 'K' ];
+	for (let suit of suits) {
+		for (let value of values) {
+			deck.push({ suit, value });
+		}
+	}
+	return deck;
+}
+
+function drawCard(deck) {
+	return deck.pop();
+}
+
+const myDeck = makeDeck();
+const card1 = drawCard(myDeck);
+
+/////
+
+const myDeck = {
+	suits: [ 'Hearts', 'Diamonds', 'Spades', 'Clubs' ],
+	values: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'A', 'K' ],
+	deck: [],
+	drawnCards: [],
+	initializeDeck() {
+		const { suits, values, deck } = this;
+		for (let suit of suits) {
+			for (let value of values) {
+				deck.push({ suit, value });
+			}
+		}
+	},
+	drawCard() {
+		const card = this.deck.pop();
+		this.drawnCards.push(card);
+		return card;
+	},
+	drawMultipleCards(num) {
+		const cardsDrawnTogether = [];
+		for (let i = 0; i < num; i++) {
+			const card = this.drawCard();
+			cardsDrawnTogether.push(card);
+		}
+		return cardsDrawnTogether;
+	}
+};
+
+const makeDeck = () => {
+	return {
+		suits: [ 'Hearts', 'Diamonds', 'Spades', 'Clubs' ],
+		values: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'A', 'K' ],
+		deck: [],
+		drawnCards: [],
+		initializeDeck() {
+			const { suits, values, deck } = this;
+			for (let suit of suits) {
+				for (let value of values) {
+					deck.push({ suit, value });
+				}
+			}
+		},
+		drawCard() {
+			const card = this.deck.pop();
+			this.drawnCards.push(card);
+			return card;
+		},
+		drawMultipleCards(num) {
+			const cardsDrawnTogether = [];
+			for (let i = 0; i < num; i++) {
+				const card = this.drawCard();
+				cardsDrawnTogether.push(card);
+			}
+			return cardsDrawnTogether;
+		}
+	};
+};
+
+const willGetDog = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const num = Math.random();
+			if (num < 0.5) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 2000);
+	});
+};
+
+willGetDog()
+	.then(() => {
+		console.log('yayyyyyy');
+	})
+	.catch(() => {
+		console.log('nooooo');
+	});
+
+const fakeRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const page = {
+				'/about': 'this is the about page',
+				'/shop': 'this is the shop page'
+			};
+			const content = page[url];
+			if (content) {
+				resolve({ status: 200, content });
+			} else {
+				reject({ status: '404' });
+			}
+		}, 2000);
+	});
+};
+
+fakeRequest('/about')
+	.then((res) => {
+		console.log('yayyyy');
+		console.log(res.status);
+		console.log(res.content);
+	})
+	.catch((res) => {
+		console.log('noooo');
+		console.log(res.status);
+	});
+
+const fakeRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const pages = {
+				'/users': [ { id: 1, username: 'Bilbo' }, { id: 5, username: 'Esmerelda' } ],
+				'/users/1': {
+					id: 1,
+					username: 'Bilbo',
+					upvotes: 360,
+					city: 'Lisbon',
+					topPostId: 454321
+				},
+				'/users/5': {
+					id: 5,
+					username: 'Esmerelda',
+					upvotes: 571,
+					city: 'Honolulu'
+				},
+				'/posts/454321': {
+					id: 454321,
+					title: 'Ladies & Gentlemen, may I introduce my pet pig, Hamlet'
+				},
+				'/about': 'This is the about page!'
+			};
+			const content = pages[url];
+			if (content) {
+				resolve({ status: 200, content });
+			} else {
+				reject({ status: 404 });
+			}
+		}, 2000);
+	});
+};
+
+fakeRequest('/users')
+	.then((res) => {
+		console.log('yesss');
+		console.log(res.status);
+		const id = res.content[0].id;
+		return fakeRequest(`/users/${id}`);
+	})
+	.then((res) => {
+		console.log(res);
+		const postId = res.content.topPostId;
+		return fakeRequest(`/posts/${postId}`);
+	})
+	.then((res) => {
+		console.log(res.content.title);
+	})
+	.catch((err) => {
+		console.log('noooo');
+		console.log(err.status);
+	});
