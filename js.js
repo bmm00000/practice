f27fd6453713b17d4eaf16977c18ff8be8e0aced
bmm00000 <bmm00000@icloud.com>
2020-06-30 18:12:18 +0200

    async and await

--- js.js
+++ js.js
@@ -369,3 +369,337 @@ fakeRequest('/users')
 		console.log('noooo');
 		console.log(err.status);
 	});
+
+const firstReq = new XMLHttpRequest();
+
+firstReq.addEventListener('load', () => {
+	console.log('it worked!');
+	const data = JSON.parse(firstReq.responseText);
+});
+firstReq.addEventListener('error', () => {
+	console.log('error!');
+});
+
+firstReq.open('GET', 'https://swapi.co/api/planets/');
+firstReq.send();
+console.log('request sent!!');
+
+const prom = fetch('https://swapi.co/api/planets/')
+	.then((response) => {
+		return response.json().then((data) => {
+			console.log(data);
+		});
+	})
+	.catch((err) => {
+		console.log('something went wrong');
+		console.log(err);
+	});
+
+async function greet() {
+	return 'hello!';
+}
+
+greet().then((val) => {
+	return 'promised resolveddd with ' + val;
+});
+
+async function sum(x, y) {
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw 'this is an errorrrrrrrr!!!!';
+	}
+	return x + y;
+}
+
+sum(1, 2)
+	.then((val) => {
+		console.log('resolved with', val);
+	})
+	.catch((err) => {
+		console.log('resolved with', err);
+	});
+
+async function getData() {
+	const res = await axios.get('jsjjjs');
+	console.log(res.data);
+}
+
+function makeColor(r, g, b) {
+	const color = {};
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	color.rgb = function() {
+		return `rgb${r}${g}${b}`;
+	};
+	return color;
+}
+
+const color1 = makeColor(2, 5, 4);
+
+function MakeColor(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+}
+
+MakeColor.prototype.rgb = function() {
+	const { r, g, b } = this;
+	return `rgb${r}${g}${b}`;
+};
+
+const color1 = new MakeColor(2, 4, 6);
+
+class MakeColor {
+	constructor(r, g, b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	rgb() {
+		const { r, g, b } = this;
+		return `rgb${r}${g}${b}`;
+	}
+	rgba(a = 1) {
+		const { r, g, b } = this;
+		return `rgb${r}${g}${b}${a}`;
+	}
+}
+
+const color1 = new MakeColor(33, 22, 11);
+
+class MakeColor {
+	constructor(r, g, b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	rgb() {
+		const {} = this;
+		return;
+	}
+}
+
+const color1 = new MakeColor(1, 2, 3);
+
+const willGetDog = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const num = Math.random();
+			if (num < 0.5) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 1000);
+	});
+};
+
+willGetDog()
+	.then(() => {
+		console.log('yesss');
+	})
+	.catch(() => {
+		console.log('noooooo');
+	});
+
+const fakeRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const page = {
+				'/user': 'This is the user page',
+				'/about': 'This is the about page'
+			};
+			const data = page[url];
+			if (data) {
+				resolve({ status: 200, data });
+			} else {
+				reject({ status: 404 });
+			}
+		}, 1000);
+	});
+};
+
+fakeRequest('/about')
+	.then((res) => {
+		console.log(res.status);
+		console.log(res.data);
+	})
+	.catch((err) => {
+		console.log(err.status);
+	});
+
+const fakeRequest = function(url) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const pages = {
+				'/about': 'This is the about page',
+				'/users': 'This is the users page'
+			};
+			const data = pages[url];
+			if (data) {
+				resolve({ status: 200, data });
+			} else {
+				reject({ status: 404 });
+			}
+		}, 1000);
+	});
+};
+
+fakeRequest()
+	.then((res) => {
+		const id = res.data[0].id;
+		return fakeRequest(`/users/${id}`);
+	})
+	.then((res) => {
+		const post = res.data.topPostId;
+		return fakeRequest(`/posts/${post}`);
+	})
+	.then((res) => {
+		console.log(res.data.title);
+	})
+	.catch((err) => {
+		console.log(err.status);
+	});
+
+const req1 = new XMLHttpRequest();
+
+req1.addEventListener('load', function() {
+	console.log('first request worked');
+	const data = JSON.parse(this.responseText);
+	const filmURL = data.results[0].films[0];
+	const req2 = new XMLHttpRequest();
+	req2.addEventListener('load', function() {
+		console.log('second request worked');
+		console.log(JSON.parse(this.responseText));
+	});
+	req2.addEventListener('error', function(e) {
+		console.log(e);
+	});
+	req2.open('GET', filmURL);
+	req2.send();
+});
+req1.addEventListener('error', () => {
+	console.log('error!');
+});
+
+req1.open('GET', 'https://swapi.dev/api/planets/');
+req1.send();
+
+fetch('https://swapi.dev/api/planets/').then((response) => {
+	response.json().then((data) => {
+		for (let planet of data.results) {
+			console.log(planet.name);
+		}
+	});
+});
+
+const checkAndParse = (response) => {
+	if (!response.ok) throw new Error('this is not right!!!');
+	return response.json();
+};
+const printPlanets = (data) => {
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data);
+};
+const get10MorePlanets = (data) => {
+	const nextPage = data.next;
+	return fetch(nextPage);
+};
+
+fetch('https://swapi.dev/api/planets/')
+	.then(checkAndParse)
+	.then(printPlanets)
+	.then(get10MorePlanets)
+	.then(checkAndParse)
+	.then(printPlanets)
+	.then(get10MorePlanets)
+	.then(checkAndParse)
+	.then(printPlanets)
+	.then(get10MorePlanets)
+	.then(checkAndParse)
+	.then(printPlanets)
+	.catch((err) => {
+		console.log('something went wrong');
+		console.log(err);
+	});
+
+axios.get('jsjsjs').then((res) => {
+	console.log(res.data);
+});
+
+axios.get('jldsjfldsfs').then(({ data }) => {
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	axios.get(data.next).then(({ data }) => {
+		for (let planet of data.results) {
+			console.log(planet.name);
+		}
+	});
+});
+
+const fetchNextPlanets = (url = 'https://lkjljl') => {
+	return axios.get(url);
+};
+const printPlanets = ({ data }) => {
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	const url = data.next;
+	return Promise.resolve(url);
+};
+
+fetchNextPlanets().then(printPlanets).then(fetchNextPlanets);
+
+async function greet() {
+	return 'Hello!';
+}
+
+greet().then((res) => {
+	console.log('my greeting is', res);
+});
+
+async function add(x, y) {
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw 'this is BS!!';
+	}
+	return x + y;
+}
+
+function add(x, y) {
+	return new Promise((resolve, reject) => {
+		if (typeof x !== 'number' || typeof y !== 'number') {
+			reject('they must be numbers!');
+		}
+		resolve(x + y);
+	});
+}
+
+add(2, 4)
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function addPlanet() {
+	return axios.get('http://jsjsjsj');
+}
+
+addPlanet().then((res) => {
+	console.log(res.data);
+});
+
+async function addPlanet() {
+	try {
+		const res = await axios.get('httpsljlsfjsl');
+		console.log(res.data);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+addPlanet();
