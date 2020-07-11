b72350449df92852add2ca6910d91afb59cd8796
bmm00000 <bmm00000@icloud.com>
2020-07-11 13:06:09 +0200

    card components

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-	return <div>Hi theree</div>;
+	return (
+		<div className="ui container comments">
+			<ApprovalCard />
+			<CommentDetail author="Sam" timeAgo="Today at 4pm" comment="Great post!" src={faker.image.avatar()} />
+			<CommentDetail
+				author="Jane"
+				timeAgo="Yesterday at 4pm"
+				comment="Fantastic post!"
+				src={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Cobra"
+				timeAgo="Yesterday at 11am"
+				comment="Mediocre post!"
+				src={faker.image.avatar()}
+			/>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
