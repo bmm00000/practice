d252d32706bd107737145120d286d69b97b5a401
bmm00000 <bmm00000@icloud.com>
2020-07-11 20:00:09 +0200

    more on express

--- node/server.js
+++ node/server.js
@@ -19,8 +19,22 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('helloo');
+	next();
+});
+
 app.get('/', (req, res) => {
-	res.send('jsdksdjflds');
+	res.send('getting root');
+});
+
+app.get('/profile', (req, res) => {
+	res.send('getting profile');
+});
+
+app.post('/', (req, res) => {
+	const user = { name: 'Sally', age: 12 };
+	res.send(user);
 });
 
 app.listen(3000);
