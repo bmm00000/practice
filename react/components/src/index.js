089905924943d2c8a03e0c552b5a39c148d4610f
bmm00000 <bmm00000@icloud.com>
2020-07-12 14:37:07 +0200

    new seasons app

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -7,20 +7,31 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<ApprovalCard />
-			<CommentDetail author="Sam" timeAgo="Today at 4pm" comment="Great post!" src={faker.image.avatar()} />
-			<CommentDetail
-				author="Jane"
-				timeAgo="Yesterday at 4pm"
-				comment="Fantastic post!"
-				src={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Cobra"
-				timeAgo="Yesterday at 11am"
-				comment="Mediocre post!"
-				src={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<div>
+					<h2>Warning!</h2>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail author="Sam" timeAgo="Today at 4pm" comment="Great post!" src={faker.image.avatar()} />
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 4pm"
+					comment="Fantastic post!"
+					src={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Cobra"
+					timeAgo="Yesterday at 11am"
+					comment="Mediocre post!"
+					src={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
