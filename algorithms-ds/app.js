051a8bab625efc08a8dce68281559eeda8466212
bmm00000 <bmm00000@icloud.com>
2020-07-14 20:07:58 +0200

    performance.now()

--- algorithms-ds/app.js
+++ algorithms-ds/app.js
@@ -9,3 +9,8 @@ function sumUp1(n) {
 function sumUp2(n) {
 	return n * (n + 1) / 2;
 }
+
+const t1 = performance.now();
+sumUp1(1000000000000);
+const t2 = performance.now();
+console.log(`Time elapsed: ${t2 - t1 / 1000} seconds.`);
