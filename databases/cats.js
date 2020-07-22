dd17b28e4938ee7f304a4ebe65146dab6332a7d7
bmm00000 <bmm00000@icloud.com>
2020-07-22 13:29:03 +0200

    adding mongoose

---
+++ databases/cats.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cat_app', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const catSchema = new mongoose.Schema({
+	name: String,
+	age: Number,
+	temperament: String
+});
+
+const Cat = mongoose.model('Cat', catSchema); // 'Cat' is the singular of the name of the collection: 'cats'
+
+// const george = new Cat({
+// 	name: 'Harris',
+// 	age: 12,
+// 	temperament: 'Evil'
+// });
+
+// george.save().then((cat) => console.log(cat));
+
+//everytime you run the code above with node, you will get a new cat
+
+//'george' is what we have in js that we are trying to save to the database. 'cat' is what's coming from the database.
+
+//ANOTHER WAY OF DOING WHAT WE DID BEFORE (create and save all at once):
+
+// Cat.create(
+// 	{
+// 		name: 'Snowwhite',
+// 		age: 22,
+// 		temperament: 'Nice'
+// 	},
+// 	function(err, cat) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log(cat);
+// 		}
+// 	}
+// );
+
+Cat.find({}, function(err, cats) {
+	if (err) {
+		console.log('Error!');
+		console.log(err);
+	} else {
+		console.log('All the cats:');
+		console.log(cats);
+	}
+});
