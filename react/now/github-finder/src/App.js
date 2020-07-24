6e4e413a70daa10d22b8199e7daf7aa238a0af2d
bmm00000 <bmm00000@icloud.com>
2020-07-24 20:02:17 +0200

    added github api

--- react/now/github-finder/src/App.js
+++ react/now/github-finder/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
+
+	async componentDidMount() {
+		this.setState({ loading: true });
+		let res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
+				.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data, loading: false });
+	}
+
 	render() {
 		return (
 			<div>
 				<Navbar title='Github Finder' icon='fa fa-github' />
 				<div className='container'>
-					<Users />
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
 		);
