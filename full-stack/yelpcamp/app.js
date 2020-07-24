e8670f187eea97b53edb2e871604776b8238adf3
bmm00000 <bmm00000@icloud.com>
2020-07-24 17:36:13 +0200

    added restful routing

--- full-stack/yelpcamp/app.js
+++ full-stack/yelpcamp/app.js
@@ -2,47 +2,86 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { urlencoded } = require('body-parser');
 
+mongoose.connect('mongodb://localhost/yelp_camp', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-//this will go to the database:
-const campgrounds = [
-	{
-		name: 'Salmon Creek',
-		image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350'
-	},
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+Campground.create(
 	{
 		name: 'Granite Hill',
-		image:
-			'https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350'
+		image: 'https://www.photosforclass.com/download/px_699558',
+		description: 'This is a huge beautiful camp.'
 	},
-	{
-		name: 'La Mateta',
-		image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350'
+	function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('New campground created');
+			console.log(campground);
+		}
 	}
-];
+);
 
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-	res.render('campgrounds', { campgrounds });
+	Campground.find({}, function(err, campgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds });
+		}
+	});
 });
 
 app.post('/campgrounds', (req, res) => {
 	let name = req.body.name;
 	let image = req.body.image;
 	let newCampground = { name: name, image: image };
-	campgrounds.push(newCampground);
-	res.redirect('/campgrounds'); //we have two 'campgrounds' routes, but the default is to redirect to the GET route
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/campgrounds');
+			//we have two 'campgrounds' routes, but the default is to redirect to the GET route
+		}
+	});
 });
 
 app.get('/campgrounds/new', (req, res) => {
 	res.render('new');
 });
 
+//watch out! the show route has to be after the new route, otherwise the new route will be triggered when you type an id in the url:
+app.get('/campgrounds/:id', (req, res) => {
+	console.log('This will be the show page');
+});
+
 app.listen(port, () => {
 	console.log(`Server listening: http://localhost:${port}`);
 });
+
+// RESTFUL ROUTES
+
+// name			url 			verb 			description
+// ===============================================================================
+// INDEX 			/dogs			GET				Display a list of all dogs.
+// NEW 			/dogs/new 			GET 			Displays form to make a new dog
+// CREATE			/dogs			POST 			Add new dog to db
+// SHOW			/dogs/:id			GET				Shows info about one dog.
