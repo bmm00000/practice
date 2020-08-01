51757f44a99e7df91afdebfe61541974d9ee4e6b
bmm00000 <bmm00000@icloud.com>
2020-08-01 14:31:02 +0200

    big O of objects and arrays

--- algorithms-ds/cs/app.js
+++ algorithms-ds/cs/app.js
@@ -1,19 +1,29 @@
 // https://cs.slides.com/colt_steele/big-o-notation#/12/0/1
 //cs.slides.com/colt_steele/
 
-https: function sumUp1(n) {
-	let total = 0;
-	for (let i = 1; i <= n; i++) {
-		total += n;
+const { func } = require('prop-types');
+
+// https: function sumUp1(n) {
+// 	let total = 0;
+// 	for (let i = 1; i <= n; i++) {
+// 		total += n;
+// 	}
+// 	return total;
+// }
+
+// function sumUp2(n) {
+// 	return n * (n + 1) / 2;
+// }
+
+// const t1 = performance.now();
+// sumUp1(1000000000000);
+// const t2 = performance.now();
+// console.log(`Time elapsed: ${t2 - t1 / 1000} seconds.`);
+
+function charCount(str) {
+	const count = {};
+	for (let char of str) {
+		!count[char] ? (count[char] = 1) : count[char]++;
 	}
-	return total;
+	return count;
 }
-
-function sumUp2(n) {
-	return n * (n + 1) / 2;
-}
-
-const t1 = performance.now();
-sumUp1(1000000000000);
-const t2 = performance.now();
-console.log(`Time elapsed: ${t2 - t1 / 1000} seconds.`);
