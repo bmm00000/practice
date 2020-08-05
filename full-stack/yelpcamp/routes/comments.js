f0e030491aa8f502abe4b68eb5b854eb560a0250
bmm00000 <bmm00000@icloud.com>
2020-08-06 00:13:07 +0200

    added edit route to comments

--- full-stack/yelpcamp/routes/comments.js
+++ full-stack/yelpcamp/routes/comments.js
@@ -45,6 +45,28 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 // we add 'isLoggedIn' as middleware here, because we want to avoid someone accessing the comments without being logged in (using postman, for example)
 
+// comments edit route
+router.get('/:comment_id/edit', (req, res) => {
+	Comment.findById(req.params.comment_id, function(err, foundComment) {
+		if (err) {
+			res.redirect('back');
+		} else {
+			res.render('comments/edit', { campground_id: req.params.id, comment: foundComment });
+		}
+	});
+});
+
+// comments update route
+router.put('/:comment_id', (req, res) => {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+		if (err) {
+			res.redirect('back');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+});
+
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
