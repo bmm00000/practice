b936677eea4b224a1f844f0fc075861d772177e8
bmm00000 <bmm00000@icloud.com>
2020-08-18 18:47:15 +0200

    add style to timer

--- vanilla-js/timer/index.js
+++ vanilla-js/timer/index.js
@@ -8,15 +8,14 @@ const circle = document.querySelector('circle');
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
-let currentOffset = 0;
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	onStart(totalDuration) {
-		console.log('Timer started...');
+		duration = totalDuration;
 	},
 
 	onTick(timeRemaining) {
-		circle.setAttribute('stroke-dashoffset', currentOffset);
-		currentOffset = currentOffset - 1;
+		circle.setAttribute('stroke-dashoffset', perimeter * timeRemaining / duration - perimeter);
 	},
 
 	onComplete() {
