670e965c30954a84573acc30305ce31169539e98
bmm00000 <bmm00000@icloud.com>
2020-09-11 14:27:53 +0200

    add styles to filenames

--- vanilla-js/node/lists2/index.js
+++ vanilla-js/node/lists2/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs'); // file system module from node standard library
 const util = require('util');
 const chalk = require('chalk');
-const path = require('path');
+const path = require('path'); // this is another module from node standard library. it will help us to find the path to specific files. otherwise, we only have strings with the name of the files and we will get an error because of it when running lstat. this module is useful to assure cross-platform compability of our code (mac, windows, etc)
 
 // in order to change file permissions for index.js, you have to type the following in the CLI (in the directory of the project): chmod +x index.js
 // next, you add a comment at the top to be able to run the file as an executable (look at the top of this file)
@@ -18,7 +18,9 @@ const path = require('path');
 // or, destructuring:
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
@@ -90,23 +92,33 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 	// }
 
 	// the third of the three options to solve the order problem (look at the screenshot with the three options):
-	// it's like a combination of the two other options:
+	// it's like a combination of the two other options(with this option you will get much better performance if you have many operations to do):
+	const statPromises = filenames.map((filename) => {
+		return lstat(path.join(targetDir, filename));
+	});
+
+	const allStats = await Promise.all(statPromises);
+
+	// for (let stats of allStats) {
+	// 	const index = allStats.indexOf(stats);
+	// 	console.log(filenames[index], stats.isFile());
+	// }
+
+	// now we want to add colours or styles to the filename, depending on whether it's a folder or not. in order to do that, we need to download 'chalk' from npm and require it (look above):
+
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+
+		if (stats.isFile()) {
+			console.log(filenames[index]);
+		} else {
+			console.log(chalk.bold(filenames[index]));
+		}
+	}
+
+	// but in the original 'ls' command, we can print the content of any directory, as far as you pass the path, for example 'ls ljdslf/ljsdlf/jlsdjf'. That's what we are going to do now:
+
+	// console.log(process.argv);
+	// if you run this, it will give you information about how the program was executed in an array or strings. if you run 'nls hellothere' it will give you 'hellothere' in the third position of the string (index 2).
+	// we will use this in the 'targetDir' (look above)
 });
-
-// const targetDir = process.argv[2] || process.cwd(); // the process module is added to the global scope of every project, so we don't have to require it. We use process.cwd becuase it has better crossplatform compability than the '.'
-
-// 	const statPromises = filenames.map((filename) => {
-// 		return lstat(path.join(targetDir, filename));
-// 	});
-
-// 	const allStats = await Promise.all(statPromises);
-
-// 	for (let stats of allStats) {
-// 		const index = allStats.indexOf(stats);
-// 		if (stats.isFile()) {
-// 			console.log(filenames[index]);
-// 		} else {
-// 			console.log(chalk.bold(filenames[index]));
-// 		}
-// 	}
-// });
