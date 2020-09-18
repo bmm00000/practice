33e20b24f3098010b908593458a6bf41193859f7
bmm00000 <bmm00000@icloud.com>
2020-09-18 22:31:47 +0200

    re-created app

--- vanilla-js/clones/movie-fight1/index.js
+++ vanilla-js/clones/movie-fight1/index.js
@@ -5,19 +5,62 @@ const fetchData = async (searchTerm) => {
 			s: searchTerm
 		}
 	});
-	console.log(response.data);
+
+	// we do the following to avoid an error if there is no movie with the name that we are looking for
+	if (response.data.Error) {
+		return [];
+	}
+
+	return response.data.Search;
 };
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+	<label><b>Search</b></label>
+	<input class="input" />
+	<div class="dropdown">
+		<div class="dropdown-menu">
+			<div class="dropdown-content results"></div>
+		</div>
+	</div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
-let timeoutId;
-const onInput = (event) => {
-	if (timeoutId) {
-		clearTimeout(timeoutId);
+// let timeoutId;
+// const onInput = (event) => {
+// 	if (timeoutId) {
+// 		clearTimeout(timeoutId);
+// 	}
+// 	timeoutId = setTimeout(() => {
+// 		fetchData(event.target.value);
+// 	}, 500);
+// };
+
+// but now we are going to write a 'debounce' function, so we can reuse the deboucing functionality for other purposes in the future (see 'utils.js')
+
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+	// console.log(movies); // this will print a promise if we don't use 'async'/'await'
+
+	// now we delete any pre-existing results, so older results don't stay there.
+	resultsWrapper.innerHTML = '';
+
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const option = document.createElement('a');
+		// now we make sure that nothing appears if there is no image poster:
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+			<img src="${imgSrc}">
+			${movie.Title}
+		`;
+		resultsWrapper.appendChild(option);
 	}
-	timeoutId = setTimeout(() => {
-		fetchData(event.target.value);
-	}, 500);
 };
 
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput));
