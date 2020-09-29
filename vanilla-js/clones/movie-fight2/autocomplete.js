7772ebe3da480153c6aad167cb11d87113d6723f
bmm00000 <bmm00000@icloud.com>
2020-09-29 21:09:11 +0100

    refactored autocomplete

--- vanilla-js/clones/movie-fight2/autocomplete.js
+++ vanilla-js/clones/movie-fight2/autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
 	root.innerHTML = `
         <label><b>Search</b></label>
         <input class="input" />
@@ -9,9 +9,9 @@ const createAutoComplete = ({ root }) => {
         </div>
     `;
 
-	const input = document.querySelector('input');
-	const dropdown = document.querySelector('.dropdown');
-	const resultsWrapper = document.querySelector('.results');
+	const input = root.querySelector('input');
+	const dropdown = root.querySelector('.dropdown');
+	const resultsWrapper = root.querySelector('.results');
 
 	// let timeoutId;
 	// const onInput = (event) => {
@@ -26,11 +26,11 @@ const createAutoComplete = ({ root }) => {
 	// but now we are going to write a 'debounce' function, so we can reuse the deboucing functionality for other purposes in the future (see 'utils.js')
 
 	const onInput = async (event) => {
-		const movies = await fetchData(event.target.value);
+		const items = await fetchData(event.target.value);
 		// console.log(movies); // this will print a promise if we don't use 'async'/'await'
 
 		// now we hide the autocomplete if there are not movies to show:
-		if (!movies.length) {
+		if (!items.length) {
 			dropdown.classList.remove('is-active');
 			return;
 		}
@@ -39,21 +39,15 @@ const createAutoComplete = ({ root }) => {
 		resultsWrapper.innerHTML = '';
 
 		dropdown.classList.add('is-active');
-		for (let movie of movies) {
+		for (let item of items) {
 			const option = document.createElement('a');
-			// now we make sure that nothing appears if there is no image poster:
-			const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
 			option.classList.add('dropdown-item');
-			option.innerHTML = `
-			<img src="${imgSrc}">
-			${movie.Title}
-		`;
+			option.innerHTML = renderOption(item);
 
 			option.addEventListener('click', () => {
 				dropdown.classList.remove('is-active');
-				input.value = movie.Title;
-				onMovieSelect(movie);
+				input.value = inputValue(item);
+				onOptionSelect(item);
 			});
 			resultsWrapper.appendChild(option);
 		}
--- vanilla-js/clones/movie-fight2/index.js
+++ vanilla-js/clones/movie-fight2/index.js
@@ -1,18 +1,35 @@
-const fetchData = async (searchTerm) => {
-	const response = await axios.get('http://www.omdbapi.com/', {
-		params: {
-			apikey: '79a0b990',
-			s: searchTerm
+createAutoComplete({
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
+		// now we make sure that nothing appears if there is no image poster:
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		return `
+			<img src="${imgSrc}">
+			${movie.Title} (${movie.Year})
+		`;
+	},
+	onOptionSelect(movie) {
+		onMovieSelect(movie);
+	},
+	inputValue(movie) {
+		return movie.Title;
+	},
+	async fetchData(searchTerm) {
+		const response = await axios.get('http://www.omdbapi.com/', {
+			params: {
+				apikey: '79a0b990',
+				s: searchTerm
+			}
+		});
+
+		// we do the following to avoid an error if there is no movie with the name that we are looking for
+		if (response.data.Error) {
+			return [];
 		}
-	});
 
-	// we do the following to avoid an error if there is no movie with the name that we are looking for
-	if (response.data.Error) {
-		return [];
+		return response.data.Search;
 	}
-
-	return response.data.Search;
-};
+});
 
 // when we click on a movie, we have to get all the info about it:
 const onMovieSelect = async (movie) => {
