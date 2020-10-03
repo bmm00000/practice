15cf56011c33850e085b9ceef4dd8f91af0be286
bmm00000 <bmm00000@icloud.com>
2020-10-03 17:22:21 +0100

    start implementing jsdom

--- vanilla-js/testing/tme/sample-project/test/forEach.test.js
+++ vanilla-js/testing/tme/sample-project/test/forEach.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { forEach } = require('../index');
 
+// we are going to implement a beforeEach function (same as mocha). keep in mind that beforeEach in mocha takes care of many corner cases that we will not care about (that's why our framework will work in only 80% of the situations), we will just make sure that our beforeEach is run before any 'it' test.
 let numbers;
 beforeEach(() => {
 	numbers = [ 1, 2, 3 ];
@@ -13,4 +14,15 @@ it('Should sum an array', () => {
 	});
 
 	assert.strictEqual(total, 6);
+
+	// now we are destroying the 'numbers' array to test the following test (meta):
+	numbers.push(3);
+	numbers.push(3);
+	numbers.push(3);
+	numbers.push(3);
+});
+
+// we are implementing a test for our testing framework (meta) lol:
+it('beforeEach is run each time', () => {
+	assert.strictEqual(numbers.length, 3);
 });
