76c7db6e7fc5b434fe37d9c7fceff41f60eb958b
bmm00000 <bmm00000@icloud.com>
2020-10-24 22:20:37 +0100

    add delete route in shopping cart

--- vanilla-js/ecomm5/routes/carts.js
+++ vanilla-js/ecomm5/routes/carts.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const carts = require('../repositories/carts');
 const cartsRepo = require('../repositories/carts');
 const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -30,11 +30,39 @@ router.post('/cart/products', async (req, res) => {
 		cart.items.push({ id: req.body.productId, quantity: 1 });
 	}
 
-	res.send('Product added');
+	await cartsRepo.update(cart.id, {
+		items: cart.items,
+	});
+
+	res.redirect('/cart');
 });
 
 // Receive a get request to show all items in the cart
+router.get('/cart', async (req, res) => {
+	if (!req.session.cartId) {
+		return res.redirect('/');
+	}
+	const cart = await cartsRepo.getOne(req.session.cartId);
+
+	for (let item of cart.items) {
+		const product = await productsRepo.getOne(item.id);
+
+		item.product = product;
+	}
+
+	res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // Receive a post request to delete an item from the cart
+router.post('/cart/products/delete', async (req, res) => {
+	const { itemId } = req.body;
+	const cart = await cartsRepo.getOne(req.session.cartId);
+
+	const items = cart.items.filter((item) => item.id !== itemId);
+
+	await cartsRepo.update(req.session.cartId, { items }); // the same as {items: items}
+
+	res.redirect('/cart');
+});
 
 module.exports = router;
