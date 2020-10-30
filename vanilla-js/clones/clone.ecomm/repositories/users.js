854c1e69fbdfece6689c94aa0ee9ad31fdfbdbb0
bmm00000 <bmm00000@icloud.com>
2020-10-30 21:50:58 +0000

    add repository-database with js

---
+++ vanilla-js/clones/clone.ecomm/repositories/users.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+class UsersRepository {
+	constructor(filename) {
+		if (!filename) {
+			throw new Error('Filename must be provided');
+		}
+
+		this.filename = filename;
+		try {
+			fs.accessSync(this.filename);
+		} catch (err) {
+			fs.writeFileSync(this.filename, '[]');
+		}
+	}
+
+	async getAll() {
+		return JSON.parse(
+			await fs.promises.readFile(this.filename, { encoding: 'utf-8' })
+		);
+	}
+
+	async create(attrs) {
+		attrs.id = this.randomId();
+		const records = await this.getAll();
+
+		records.push(attrs);
+
+		await this.writeAll(records);
+	}
+
+	async writeAll(records) {
+		await fs.promises.writeFile(
+			this.filename,
+			JSON.stringify(records, null, 2)
+		);
+	}
+
+	randomId() {
+		return crypto.randomBytes(4).toString('hex');
+	}
+
+	async getOne(id) {
+		const records = await this.getAll();
+
+		return records.find((record) => record.id === id);
+	}
+
+	async delete(id) {
+		const records = await this.getAll();
+
+		const filteredRecords = records.filter((record) => record.id !== id);
+
+		await this.writeAll(filteredRecords);
+	}
+
+	// STILL NEED TO DO UPDATE AND GETONEBY!!!
+}
+
+const test = async () => {
+	const repo = new UsersRepository('users.json');
+	await repo.delete('4e6c533a');
+};
+
+test();
