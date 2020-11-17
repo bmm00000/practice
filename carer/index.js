9db497bcdd3c21814159dbca79b30e2f80d9647f
bmm00000 <bmm00000@icloud.com>
2020-11-17 00:33:55 +0000

    add both client and server based validation with bootstrap and joi

--- carer/index.js
+++ carer/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { carerSchema } = require('./schemas');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
 const Carer = require('./models/carer');
@@ -27,45 +30,87 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCarer = (req, res, next) => {
+	const { error } = carerSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(',');
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('/carers', async (req, res) => {
-	const carers = await Carer.find({});
-	res.render('carers/index', { carers });
-});
+app.get(
+	'/carers',
+	catchAsync(async (req, res) => {
+		const carers = await Carer.find({});
+		res.render('carers/index', { carers });
+	})
+);
 
 app.get('/carers/new', (req, res) => {
 	res.render('carers/new');
 });
 
-app.post('/carers', async (req, res) => {
-	const carer = new Carer(req.body.carer);
-	await carer.save();
-	res.redirect(`/carers/${carer._id}`);
+app.post(
+	'/carers',
+	validateCarer,
+	catchAsync(async (req, res, next) => {
+		// if (!req.body.carer) throw new ExpressError('Invalid carer data...', 400);
+
+		const carer = new Carer(req.body.carer);
+		await carer.save();
+		res.redirect(`/carers/${carer._id}`);
+	})
+);
+
+app.get(
+	'/carers/:id',
+	catchAsync(async (req, res) => {
+		const carer = await Carer.findById(req.params.id);
+		res.render('carers/show', { carer });
+	})
+);
+
+app.get(
+	'/carers/:id/edit',
+	catchAsync(async (req, res) => {
+		const carer = await Carer.findById(req.params.id);
+		res.render('carers/edit', { carer });
+	})
+);
+
+app.put(
+	'/carers/:id',
+	validateCarer,
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const carer = await Carer.findByIdAndUpdate(id, { ...req.body.carer });
+		res.redirect(`/carers/${carer._id}`);
+	})
+);
+
+app.delete(
+	'/carers/:id',
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		await Carer.findByIdAndDelete(id);
+		res.redirect('/carers');
+	})
+);
+
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page not found...', 404));
 });
 
-app.get('/carers/:id', async (req, res) => {
-	const carer = await Carer.findById(req.params.id);
-	res.render('carers/show', { carer });
-});
-
-app.get('/carers/:id/edit', async (req, res) => {
-	const carer = await Carer.findById(req.params.id);
-	res.render('carers/edit', { carer });
-});
-
-app.put('/carers/:id', async (req, res) => {
-	const { id } = req.params;
-	const carer = await Carer.findByIdAndUpdate(id, { ...req.body.carer });
-	res.redirect(`/carers/${carer._id}`);
-});
-
-app.delete('/carers/:id', async (req, res) => {
-	const { id } = req.params;
-	await Carer.findByIdAndDelete(id);
-	res.redirect('/carers');
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Something went wrong...';
+	res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
