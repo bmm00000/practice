d9c9f3bff19d465d6e442208828268695ce76c0f
bmm00000 <bmm00000@icloud.com>
2020-11-22 19:33:53 +0000

    set up for big o practice

--- algorithms-ds/cs/app.js
+++ algorithms-ds/cs/app.js
@@ -1,29 +1,21 @@
-// https://cs.slides.com/colt_steele/big-o-notation#/12/0/1
-//cs.slides.com/colt_steele/
-
-const { func } = require('prop-types');
-
-// https: function sumUp1(n) {
-// 	let total = 0;
-// 	for (let i = 1; i <= n; i++) {
-// 		total += n;
-// 	}
-// 	return total;
-// }
-
-// function sumUp2(n) {
-// 	return n * (n + 1) / 2;
-// }
-
-// const t1 = performance.now();
-// sumUp1(1000000000000);
-// const t2 = performance.now();
-// console.log(`Time elapsed: ${t2 - t1 / 1000} seconds.`);
-
-function charCount(str) {
-	const count = {};
+function reverseStr(str) {
+	let newStr = '';
 	for (let char of str) {
-		!count[char] ? (count[char] = 1) : count[char]++;
+		newStr = char + newStr;
 	}
-	return count;
+	return newStr;
 }
+
+function sumUp(n) {
+	let total = 0;
+	for (let i = 1; i <= n; i++) {
+		total += i;
+	}
+	return total;
+}
+
+const t1 = performance.now();
+// sumUp(1000000000);
+// const t2 = performance.now();
+
+// console.log(t2 - t1);
