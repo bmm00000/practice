4a05c625c630f5cfbcbc9bf9746ebe4b249a1587
bmm00000 <bmm00000@icloud.com>
2020-11-23 22:37:44 +0000

    add register and login routes

--- carer/index.js
+++ carer/index.js
@@ -6,9 +6,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const carers = require('./routes/carers');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const carerRoutes = require('./routes/carers');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/carer', {
 	useNewUrlParser: true,
@@ -46,14 +50,22 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error');
 	next();
 });
 
-app.use('/carers', carers);
-app.use('/carers/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/carers', carerRoutes);
+app.use('/carers/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
 	res.render('home');
