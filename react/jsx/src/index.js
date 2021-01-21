c456549ab20176e9cd818ed6538726eea91d06a8
bmm00000 <bmm00000@icloud.com>
2021-01-21 18:50:31 +0100

    differences between jsx and html

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function clickHere() {
+	return 'Click here!';
+}
+
 const App = () => {
-	return <div>Hi there!</div>;
+	const title = 'Hello everyone';
+	const labelText = { text: 'Enter your name' };
+	return (
+		<div>
+			<h1>{title}</h1>
+			<label className='first-label' htmlFor='name'>
+				{labelText.text}
+			</label>
+			<input type='text' id='name' />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>
+				{clickHere()}
+			</button>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
