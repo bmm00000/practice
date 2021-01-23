f5a8f43d0a78cb3f2b7c440f6d1fc3e9fd21dd3d
bmm00000 <bmm00000@icloud.com>
2021-01-23 16:12:03 +0100

    props system

---
+++ react/components/src/CommentDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const CommentDetail = (props) => {
+	return (
+		<div class='comment'>
+			<a class='avatar'>
+				<img src={props.imgSrc} />
+			</a>
+			<div class='content'>
+				<a class='author'>{props.author}</a>
+				<div class='metadata'>
+					<div class='date'>{props.timeAgo}</div>
+					<div class='rating'>
+						<i class='star icon'></i>
+						{props.faves}
+					</div>
+				</div>
+				<div class='text'>{props.text}</div>
+			</div>
+		</div>
+	);
+};
+
+export default CommentDetail;
