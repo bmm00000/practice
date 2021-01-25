5f3879384b58474eba82523b1325c6bd5e366002
bmm00000 <bmm00000@icloud.com>
2021-01-25 15:24:31 +0100

    setState in class components

--- react/seasons/src/index.js
+++ react/seasons/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position),
-		(err) => console.log(err)
-	);
-	return <div>hello there</div>;
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { lat: null, errMessage: '' };
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude });
+			},
+			(err) => {
+				this.setState({ errMessage: err.message });
+			}
+		);
+	}
+	// React says we have to define 'render'!
+	render() {
+		return (
+			<div>
+				Latitude: {this.state.lat} <br />
+				Error: {this.state.errMessage}
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
