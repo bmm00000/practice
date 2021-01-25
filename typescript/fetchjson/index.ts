bf29a99bd7210b10a0bb5075c33e45cd68be9779
bmm00000 <bmm00000@icloud.com>
2021-01-25 19:35:35 +0100

    first interface

--- typescript/fetchjson/index.ts
+++ typescript/fetchjson/index.ts
@@ -2,12 +2,24 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-	const id = response.data.id;
-	const title = response.data.title;
-	const completed = response.data.completed;
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
 
-	console.log(
-		`The todo with id ${id}, with title ${title}: is completed? ${completed}`
-	);
+axios.get(url).then((response) => {
+	const todo = response.data as Todo;
+
+	const id = todo.id;
+	const title = todo.title;
+	const completed = todo.completed;
+
+	conLog(id, title, completed);
 });
+
+function conLog(id: number, title: string, completed: boolean) {
+	console.log(
+		`The todo with id ${id}, with title '${title}': is completed? ${completed}`
+	);
+}
