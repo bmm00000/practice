cdb482c6e52e3b7d986ba20b7f4a557c75773d2f
bmm00000 <bmm00000@icloud.com>
2021-03-10 15:55:24 +0100

    refactor

--- legacy/sp-test/app.js
+++ legacy/sp-test/app.js
@@ -1,97 +1,105 @@
 const fs = require('fs');
 
 try {
-	// FIX THE PATH NAME!!
-	// REFACTOR [0] WITH ARRAY DECONSTRUCTION
-	const rawData = fs.readFileSync(
-		'/Users/Boix/Desktop/practice/legacy/sp-test/webserver.log',
-		'utf8'
-	);
+	const rawData = fs.readFileSync(process.env.PWD + '/webserver.log', 'utf8');
 
-	const dataArr = [];
-	const arr = rawData.split('\n');
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === '') {
-			continue;
+	const parseData = (rawData) => {
+		const data = [];
+		const entries = rawData.split('\n');
+		for (let i = 0; i < entries.length; i++) {
+			if (entries[i] === '') {
+				continue;
+			}
+			const entry = entries[i].split(' ');
+			data.push(entry);
 		}
-		const inArr = arr[i].split(' ');
-		dataArr.push(inArr);
-	}
+		return data;
+	};
 
-	// List of webpages with total page views ordered from most to least page views:
-
-	const logSummary = {};
-	dataArr.forEach((elem) => {
-		if (logSummary[elem[0]]) {
-			logSummary[elem[0]]++;
-		} else {
-			logSummary[elem[0]] = 1;
-		}
-	});
-
-	// POSSIBLE A HELPER FUNCTION TO ORDER??
-	const sorting = [];
-	for (let path in logSummary) {
-		sorting.push([path, logSummary[path]]);
-	}
-
-	sorting.sort(function (a, b) {
-		return b[1] - a[1];
-	});
-
-	console.log(
-		'List of webpages with total page views ordered from most to least page views:'
-	);
-
-	sorting.forEach((pair) => {
-		const [path, visits] = pair;
-		console.log(`${path}: ${visits} visits.`);
-	});
-
-	// List of webpages with total unique page views also ordered from most to least page views:
-
-	const usersLogSummary = {};
-	dataArr.forEach((elem) => {
-		if (usersLogSummary[elem[0]]) {
-			usersLogSummary[elem[0]].push(elem[1]);
-		} else {
-			const pathUsers = [];
-			pathUsers.push(elem[1]);
-			usersLogSummary[elem[0]] = pathUsers;
-		}
-	});
-
-	const uniqueUsersLogSummary = {};
-	for (let path in usersLogSummary) {
-		const uniqueUsers = usersLogSummary[path].filter((item, index) => {
-			return usersLogSummary[path].indexOf(item) === index;
+	const getTotalViews = (data) => {
+		const totalViews = {};
+		data.forEach((entry) => {
+			const [path] = entry;
+			if (totalViews[path]) {
+				totalViews[path]++;
+			} else {
+				totalViews[path] = 1;
+			}
 		});
-		uniqueUsersLogSummary[path] = uniqueUsers;
-	}
+		return totalViews;
+	};
 
-	const uniqueVisits = {};
-	for (let path in uniqueUsersLogSummary) {
-		uniqueVisits[path] = uniqueUsersLogSummary[path].length;
-	}
+	const getUsersByPath = (data) => {
+		const usersByPath = {};
+		data.forEach((entry) => {
+			const [path, ip] = entry;
+			if (usersByPath[path]) {
+				usersByPath[path].push(ip);
+			} else {
+				const pathUsers = [];
+				pathUsers.push(ip);
+				usersByPath[path] = pathUsers;
+			}
+		});
+		return usersByPath;
+	};
 
-	// POSSIBLE A HELPER FUNCTION TO ORDER??
-	const sortingT = [];
-	for (let path in uniqueVisits) {
-		sortingT.push([path, uniqueVisits[path]]);
-	}
+	const getUniqueUsersByPath = (usersByPath) => {
+		const uniqueUsersByPath = {};
+		for (let path in usersByPath) {
+			const uniqueUsers = usersByPath[path].filter((item, index) => {
+				return usersByPath[path].indexOf(item) === index;
+			});
+			uniqueUsersByPath[path] = uniqueUsers;
+		}
+		return uniqueUsersByPath;
+	};
 
-	sortingT.sort(function (a, b) {
-		return b[1] - a[1];
-	});
+	const getUniqueViews = (uniqueUsersByPath) => {
+		const uniqueViews = {};
+		for (let path in uniqueUsersByPath) {
+			uniqueViews[path] = uniqueUsersByPath[path].length;
+		}
+		return uniqueViews;
+	};
 
-	console.log(
-		'List of webpages with total unique page views also ordered from most to least page views:'
-	);
+	const order = (views) => {
+		const sorting = [];
+		for (let path in views) {
+			sorting.push([path, views[path]]);
+		}
 
-	sortingT.forEach((pair) => {
-		const [path, number] = pair;
-		console.log(`${path}: ${number} unique visits.`);
-	});
+		sorting.sort(function (a, b) {
+			return b[1] - a[1];
+		});
+
+		return sorting;
+	};
+
+	const printReport = (message, orderedArr) => {
+		console.log(message);
+		orderedArr.forEach((entry) => {
+			const [path, visits] = entry;
+			console.log(`${path}: ${visits} visits.`);
+		});
+	};
+
+	const data = parseData(rawData);
+	const totalViews = getTotalViews(data);
+	const usersByPath = getUsersByPath(data);
+	const uniqueUsersByPath = getUniqueUsersByPath(usersByPath);
+	const uniqueViews = getUniqueViews(uniqueUsersByPath);
+
+	const orderedTotal = order(totalViews);
+	const orderedUnique = order(uniqueViews);
+
+	const messageTotalViews =
+		'List of webpages with total page views ordered from most to least page views:';
+	const messageUniqueViews =
+		'List of webpages with total unique page views also ordered from most to least page views:';
+
+	printReport(messageTotalViews, orderedTotal);
+	printReport(messageUniqueViews, orderedUnique);
 } catch (err) {
 	console.error(err);
 }
