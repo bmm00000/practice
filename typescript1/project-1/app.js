3dcc80e1b4f252b91efffedcf929d53eb477c70b
bmm00000 <bmm00000@icloud.com>
2021-06-22 18:20:25 +0100

    jasmine setup

--- typescript1/project-1/app.js
+++ typescript1/project-1/app.js
@@ -1,12 +1,5 @@
-function add(num1, num2, showResult, text) {
-    if (showResult) {
-        var result = num1 + num2;
-        console.log(text + result);
-    }
-}
-// const n1 = '3';
-var n1 = 2;
-var n2 = 3;
-var printResult = true;
-var intro = 'The result is: ';
-add(n1, n2, printResult, intro);
+var me = {
+    name: 'Jose',
+    age: 29
+};
+console.log(me.name);
---
+++ typescript1/project-1/app.ts
@@ -0,0 +1,6 @@
+const me = {
+	name: 'Jose',
+	age: 29,
+};
+
+console.log(me.name);
