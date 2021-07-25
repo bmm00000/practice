3a450ffaed1e1e786bed634c530fac9e78a695bc
Jose Boix <bmm00000@icloud.com>
2021-07-25 21:48:09 +0100

    start addingh dynamic rendering

--- react1/expense-tracker/intermediate/expense-tracker1/src/components/Expenses/Expenses.js
+++ react1/expense-tracker/intermediate/expense-tracker1/src/components/Expenses/Expenses.js
@@ -8,9 +8,16 @@ import './Expenses.css';
 const Expenses = ({ expenses }) => {
 	const [filteredYear, setFilteredYear] = useState('2021');
 
+	const [filteredExpenses, setFilteredExpenses] = useState(expenses);
+
 	const filterChangeHandler = (selectedYear) => {
 		setFilteredYear(selectedYear);
 		// if you console.log the year here, it will appear an old state because setState doesn't change the value striaghtaway, but schedules the state update
+		setFilteredExpenses((prevExpenses) => {
+			return prevExpenses.filter(
+				(expense) => expense.date.getFullYear() === filteredYear
+			);
+		});
 	};
 
 	return (
@@ -20,7 +27,7 @@ const Expenses = ({ expenses }) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{expenses.map((expense) => (
+				{filteredExpenses.map((expense) => (
 					<ExpenseItem
 						key={expense.id}
 						title={expense.title}
