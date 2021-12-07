a660e4e7f6481f57f5b22c3a5b4adad2c541d67b
Jose Boix <bmm00000@icloud.com>
2021-12-07 00:06:15 +0000

    edit notes from meetup clone

--- react1/summary/main-project/meetup-clone/src/components/meetups/MeetupList.js
+++ react1/summary/main-project/meetup-clone/src/components/meetups/MeetupList.js
@@ -1,6 +1,7 @@
 import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
+// we could write the code to fetch meetups in this component, but we want to make this component reusable (actually we will use this component in different pages), so we won't fetch the data here.
 function MeetupList(props) {
 	return (
 		<ul className={classes.list}>
