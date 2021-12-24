4c373d5a01d2c6d8e107b92249c07841082762f9
Jose Boix <bmm00000@icloud.com>
2021-12-24 19:01:47 +0100

    add users list and add users component toreact users-app

--- react1/users-app/src/App.js
+++ react1/users-app/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+const USERS_DUMMY = [
+	{ name: 'Pedro', age: 54, id: 1 },
+	{ name: 'Albert', age: 22, id: 2 },
+];
 
 function App() {
+	const [usersList, setUsersList] = useState(USERS_DUMMY);
+
+	const addUserHandler = (user) => {
+		setUsersList((prevUsers) => {
+			const updatedUsers = [...prevUsers];
+			return updatedUsers.unshift(user);
+		});
+	};
+
 	return (
 		<div>
-			<AddUser />
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
 		</div>
 	);
 }
--- react1/users-app/src/components/Users/AddUser.js
+++ react1/users-app/src/components/Users/AddUser.js
@@ -5,19 +5,35 @@ import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-	const [userName, setUserName] = useState('');
-	const [userAge, setUserAge] = useState('');
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
+	// anything that is entered as an input is always retrieved as a string (that's how js and the dom work), that's why we initialize 'enteredAge' as an empty string
 
-	const addUserNameHandler = (event) => {
-		setUserName(event.target.value);
+	const usernameChangeHandler = (event) => {
+		setEnteredUsername(event.target.value);
 	};
 
-	const addUserAgeHandler = (event) => {
-		setUserAge(event.target.value);
+	const ageChangeHandler = (event) => {
+		setEnteredAge(event.target.value);
 	};
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+		if (+enteredAge < 1) {
+			return;
+		}
+		const newUser = {
+			name: enteredUsername,
+			age: enteredAge,
+			id: Math.random().toString(),
+		};
+
+		props.onAddUser(newUser);
+		setEnteredUsername('');
+		setEnteredAge('');
 	};
 
 	return (
@@ -27,15 +43,16 @@ const AddUser = (props) => {
 				<input
 					type='text'
 					id='username'
-					value={userName}
-					onChange={addUserNameHandler}
+					value={enteredUsername}
+					// we add the 'value', so we have the current state to be reflected here every time that this component is rendered. if we only wanted to change the input content via keystrokes, we would not need to do this. however, we also want to change the input content when we submit the form (we want to make it empty). that's why we need to do this.
+					onChange={usernameChangeHandler}
 				/>
 				<label htmlFor='age'>Age</label>
 				<input
 					type='number'
 					id='age'
-					value={userAge}
-					onChange={addUserAgeHandler}
+					value={enteredAge}
+					onChange={ageChangeHandler}
 				/>
 				<Button type='submit'>Add User</Button>
 			</form>
