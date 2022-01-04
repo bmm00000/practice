f2666762a4ec033dbed01708f05c9fe277c73351
Jose Boix <bmm00000@icloud.com>
2022-01-04 18:36:18 +0100

    add refs to form inputs

--- react1/users-app/app/src/components/UI/ErrorModal.js
+++ react1/users-app/app/src/components/UI/ErrorModal.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Card from './Card';
 import Button from '../UI/Button';
 import classes from './ErrorModal.module.css';
-import React from 'react';
 
 const ErrorModal = (props) => {
-	return (
-		<React.Fragment>
-			<div className={classes.backdrop} onClick={props.onConfirm} />
+	// we want to tell react that the backdrop should be portaled somewhere. in order to do that, we create a new component, and we add it in this file, because in this app we only use the Backdrop component in conjunction with the modal (but you could put it in a different file, especially if you would need to use the Backdrop component in conjunction with other components as well):
+
+	const Backdrop = (props) => {
+		return <div className={classes.backdrop} onClick={props.onConfirm} />;
+	};
+
+	const ModalOverlay = (props) => {
+		return (
 			<Card className={classes.modal}>
 				<header className={classes.header}>
 					<h2>{props.title}</h2>
@@ -18,11 +25,33 @@ const ErrorModal = (props) => {
 					<Button onClick={props.onConfirm}>Ok</Button>
 				</footer>
 			</Card>
+		);
+	};
+
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById('backdrop-root')
+			)}
+			{/* when calling the createPortal method, it takes two arguments: your react node that should be rendered, and a pointer (dom api provided by the browser) to the real dom where this container whould be rendered in. all and all, what we are doing is moving/portaling the html that is about to be rendered to a different place. as a result, no matter where you use the ErrorModal component (no matter how deeply nested it is in other elements), the Backdrop component will be always rendered where we specified. in the JSX code, we place the ErrorModal wherever we need (as nested as it can be), but the html code is rendered in a place that is semantically correct. therefore, instead of using the Backdrop in the JSX block, you use {ReactDOM.createPortal(<Backdrop.....)}, so you will decide where the final html will be rendered in the dom. as a result, the organization of the JSX doesn't change */}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={props.title}
+					message={props.message}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById('overlay-root')
+			)}
 		</React.Fragment>
 	);
 };
 
 export default ErrorModal;
 
-// if you inspect in the browser, you will see that the modal is rendered next to the input form (at the same level), this is semantically incorrect, since the modal should refer to the whole page. in order to make it semantically correct, you may want to have the backdrop and modal as direct childs of the body, next to the 'root' div that holds the rest of our app.
-// portals need two things: you need a place you want to port the component to, and then you need to let the component know that it should have a portal to that place => you add divs with ids in the html file (you can add different such roots for different kinds of components that should be portaled there, or you can just add an id='overlay-root' which will hold all sorts of overlays, not only modals, but also side-drawers, etc.)
+// if you trigger the modal manually and inspect in the browser, you will see that the modal is rendered next to the input form (at the same level), this is semantically incorrect, since the modal should refer to the whole page. in order to make it semantically correct, you may want to have the modal as a direct child of the body, next to the 'root' div that holds the rest of our app. (you can try this out by dragging and dropping the dom elements when you inspect in the browser)
+// portals need two things: you need a place you want to port the component to (we include a div in the html file), and then you need to let the component know that it should have a portal to that place => you add divs with ids in the html file (you can add different such roots for different kinds of components that should be portaled there, or you can just add an id='overlay-root' which will hold all sorts of overlays, not only modals, but also side-drawers, etc.)
+
+// you can think of the 'react' library as having all the react features, and the 'react-dom' library uses the 'react' library to bring these features into the web browser (to make them compatible with the DOM) (in other words, the 'react' library doesn't have if you run it in an environment that has a dom, or if you would use it to build a native app, ie. you can use 'react native' in conjunction with 'react' to build a mobile app). therefore, 'react-dom' is the adaptor of 'react' to the browser.
+
+// in summary, fragments and portals are two features that help us write cleaner html code (the app worked the same way before we added those features, but now our code is semantically better).
--- react1/users-app/app/src/components/Users/AddUser.js
+++ react1/users-app/app/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Wrapper from '../Helpers/Wrapper';
 import Card from '../UI/Card';
@@ -7,31 +7,41 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-	const [enteredUsername, setEnteredUsername] = useState('');
-	const [enteredAge, setEnteredAge] = useState('');
+	const nameInputRef = useRef();
+	// it returns a value which allow us to work with that element to which we are going to connect it.
+	const ageInputRef = useRef();
+	// they are initialized to be undefined because that's the default (you could add an argument to initialize the ref with). then we will let react know that we want to connect it with an html element, by adding the special prop 'ref' to that element. the first time that react reaches the render block, it will set the value stored in 'nameInputRef' and 'ageInputRef' to the native dom element that is rendered based on the JSX buit-in 'input' element that we have here (eg. what will end up in 'nameInputRef' is a real dom element). specifially, it 'nameInputRef' will contain an object that has a 'current' prop, which holds the actual value that ref is connected with. by default it's undefined, but as soon as the return block runs, the 'nameInputRef' is connected to the html input element (the actual dom node, which you can now manipulate, etc.), and this is stored in the 'current' prop of the object. it is recommended that you don't manipulate that dome node, since we use react to do that. however, we can use this feature to read data (we are not changing anything by reading data. in our example, since it's an input element, we can access 'current.value' to access the data of the input). therefore, we can just read the data when the submit button is pressed, so we don't need to be updating state every time that the user presss a key.
+	// however, doing this we lose our resetting logic (to make the inputs blank after user submits the form). to fix this, we have to options: go back to the approach based on state, or we can do something which we should rarely do (a bit hacky), but which is ok here in the context of an input value that we want to reset (manipulate the dom without react: we said we shouldn't do that, but if we just want to reset the value entered by a user, it's something you can consider doing (we are not adding a new element or changing a css class, etc.)): below we add: nameInputRef.current.value = '', and ageInputRef.current.value = ''
+	// in a nutshell: if you have a case where you just want to read a value, and you never plan on changing anything, then you don't really need state, since it's a lot of unecessary code, so refs are probably better. but in any case either state or refs are fine as approaches, so you have to decide depending on the circumstances. in our case: refs are a bit less code, but we have the edge case of manipulating the dom to reset the input values. Or the state approch, that is a bit cleaner, but we use more code.
+
+	// const [enteredUsername, setEnteredUsername] = useState('');
+	// const [enteredAge, setEnteredAge] = useState('');
 	// anything that is entered as an input is always retrieved as a string (that's how js and the dom work), that's why we initialize 'enteredAge' as an empty string
 	const [error, setError] = useState();
 	// initially, our error is undefined.
 
-	const usernameChangeHandler = (event) => {
-		setEnteredUsername(event.target.value);
-	};
+	// const usernameChangeHandler = (event) => {
+	// 	setEnteredUsername(event.target.value);
+	// };
 
-	const ageChangeHandler = (event) => {
-		setEnteredAge(event.target.value);
-	};
+	// const ageChangeHandler = (event) => {
+	// 	setEnteredAge(event.target.value);
+	// };
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
 
-		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+		const enteredName = nameInputRef.current.value;
+		const enteredUserAge = ageInputRef.current.value;
+
+		if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
 			setError({
 				title: 'Fill in the inputs!',
 				message: 'Sorry, the inputs cannot be empty like that...',
 			});
 			return;
 		}
-		if (+enteredAge < 1) {
+		if (+enteredUserAge < 1) {
 			setError({
 				title: 'Negative age?',
 				message: 'Sorry, the age cannot be negative like that...',
@@ -45,15 +55,17 @@ const AddUser = (props) => {
 		// 	id: Math.random().toString(),
 		// };
 
-		props.onAddUser(enteredUsername, enteredAge);
-		setEnteredUsername('');
-		setEnteredAge('');
+		props.onAddUser(enteredName, enteredUserAge);
+		// setEnteredUsername('');
+		// setEnteredAge('');
+		nameInputRef.current.value = '';
+		ageInputRef.current.value = '';
 	};
 
 	const errorHandler = () => {
 		setError(null);
-		setEnteredUsername('');
-		setEnteredAge('');
+		// setEnteredUsername('');
+		// setEnteredAge('');
 	};
 
 	return (
@@ -71,16 +83,18 @@ const AddUser = (props) => {
 					<input
 						type='text'
 						id='username'
-						value={enteredUsername}
+						// value={enteredUsername}
 						// we add the 'value', so we have the current state to be reflected here every time that this component is rendered. if we only wanted to change the input content via keystrokes, we would not need to do this. however, we also want to change the input content when we submit the form (we want to make it empty). that's why we need to do this.
-						onChange={usernameChangeHandler}
+						// onChange={usernameChangeHandler}
+						ref={nameInputRef}
 					/>
 					<label htmlFor='age'>Age</label>
 					<input
 						type='number'
 						id='age'
-						value={enteredAge}
-						onChange={ageChangeHandler}
+						// value={enteredAge}
+						// onChange={ageChangeHandler}
+						ref={ageInputRef}
 					/>
 					<Button type='submit'>Add User</Button>
 				</form>
@@ -102,4 +116,8 @@ export default AddUser;
 
 // remember, the syntax <> is not html, and it's not valid JSX in all projects (your project setup needs to support it). when you use this built-in wrapper, the divs that were previously rendered into the DOM, are not rendered anymore (so the html that will be rendered is cleaner). in any case, you can always use React.Fragment, or just Fragment (if you import {Fragment} from 'react').
 
-// react portals also help us to write cleaner code. having the nested modal, apart from being not semantically correct, it can also lead to real problems, with styling or accessibility (if a screenreader has to interpret the html code, it will not see the modal as an overlay for the whole page). we can use a portal in order to keep the structure we have on the left side of the slide (so we can organize the components the way we want to, so we don't have any problems when passing data and so on), but render the html in a semantially correct way.
+// react portals also help us to write cleaner code. having the nested modal, apart from being not semantically correct, it can also lead to real problems, with styling or accessibility (if a screenreader has to interpret the html code, it will not see the modal as an overlay for the whole page). we can use a portal in order to keep the react code structure we have on the left side of the slide (so we can organize the components the way we want to, so we don't have any problems when passing data and so on), but render the html in a semantially correct way.
+
+// refs allow us to get access to dom elements and work with them. for example, so far, we are updating state every time the user presses a key, but this doesn't sound like the most efficient option. refs will help us with this. refs will establish a connection between the html element that is rendered at the end, and the other js code.
+
+// the approach of using refs to interact with dom elements, specifically with input elements, has a special name: we are talking about 'uncontrolled components' if you are accesing values with a ref. therefore, our inputs would now be uncontrolled components, because their interal state (the value reflected in them) is not controlled by react (we rely on the default behaviour of the inputs, and then we just fetch it with a react feature, but we don't feed data back into the input). (when we reset the inputs, we are not using react). so we are not controlling the state of the input element with react. you can talk about controlled and uncontrolled components in the context of any component, but usually we have this scenario when we talk about input components in a form in general, because these components usually have an interal state natively by the browser, and when we want to work with those components in a react app, we want to connect our react state to that. when we use refs, we have uncontrolled components, and when we use state we have controlled components (then, the inputs internal state is controlled by react).
