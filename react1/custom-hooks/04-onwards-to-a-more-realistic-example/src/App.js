8c6ac7bfc15e1870e509f823efb43368b6f9aeda
Jose Boix <bmm00000@icloud.com>
2022-03-23 21:28:33 +0000

    add useHttp custom hook to react project

--- react1/custom-hooks/04-onwards-to-a-more-realistic-example/src/App.js
+++ react1/custom-hooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,59 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+	const transformTasks = useCallback((tasksObj) => {
+		const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+		for (const taskKey in tasksObj) {
+			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+		}
 
-      const data = await response.json();
+		setTasks(loadedTasks);
+	}, []);
 
-      const loadedTasks = [];
+	const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+	// remember, when we use a custom hook that uses state in a component, this component will implicitly use the state set up in the custom hook, so the state configured in the custom hook is attached to the component where we use the custom hook. in our case, every time the App component is re-evaluated, we will call the custom hook again, and when that happens we will re-create the sendRequest function, therefore return a new function object (fetchTasks), and useEffect will run again if we added fetchTasks in the dependencies array (infinite loop). (this happens because functions are objects in js, and every time a function is re-created, even if it contains the same logic, it's a brand new object in memory). That's why we useCallback in the custom hook.
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+	useEffect(() => {
+		fetchTasks({
+			url: 'https://custom-hooks-b5e68-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+		});
+	}, []);
 
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+	const taskAddHandler = (task) => {
+		setTasks((prevTasks) => prevTasks.concat(task));
+	};
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
-  return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ react1/custom-hooks/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+
+const useHttp = (applyData) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const sendRequest = useCallback(
+		async (requestConfig) => {
+			setIsLoading(true);
+			setError(null);
+			try {
+				const response = await fetch(requestConfig.url, {
+					method: requestConfig.method ? requestConfig.method : 'GET',
+					headers: requestConfig.headers ? requestConfig.headers : {},
+					body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+				});
+
+				if (!response.ok) {
+					throw new Error('Request failed!');
+				}
+
+				const data = await response.json();
+				// we are assuming that we will always receive json data, so we will not make this hook super generic to work with any kind of data.
+				applyData(data);
+			} catch (err) {
+				setError(err.message || 'Something went wrong!');
+			}
+			setIsLoading(false);
+		},
+		[applyData]
+	);
+	// watch out! requestConfig is an object, and applyData, as a function, is also an object. therefore, in the App component we have to make sure that these objects are not recreated every time App is re-evaluated. that's why we have to useCallback in the App component as well.
+
+	// return { isLoading: isLoading, error: error, sendRequest: sendRequest };
+	// the former is the same as:
+	return { isLoading, error, sendRequest };
+};
+
+export default useHttp;
