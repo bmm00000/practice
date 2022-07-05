bb30bd49d165c350ea56aff2417e1905040fd3f3
Jose Boix <bmm00000@icloud.com>
2022-07-05 14:02:20 +0100

    practice food order app

---
+++ react1/6-food-order/02-starting-project/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+import { Fragment } from 'react';
+
+function App() {
+	return (
+		<Fragment>
+			<Header />
+			<Meals />
+		</Fragment>
+	);
+}
+
+export default App;
