06f530b3dc7a670903cc620b78a6d00dc01b4416
Jose Boix <bmm00000@icloud.com>
2022-07-13 18:34:12 +0100

    add AddMovie component to react app

--- react1/9-http-requests/revision/01-starting-project/src/App.js
+++ react1/9-http-requests/revision/01-starting-project/src/App.js
@@ -1,34 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	const getMoviesHandler = useCallback(async () => {
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch('https://swapi.py4e.com/api/films');
+
+			if (!response.ok) {
+				throw new Error('We got an error!');
+			}
+
+			const data = await response.json();
+			const transformedMovies = data.results.map((movie) => {
+				return {
+					id: movie.episode_id,
+					title: movie.title,
+					releaseDate: movie.release_date,
+					openingText: movie.opening_crawl,
+				};
+			});
+
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
+	}, []);
+
+	useEffect(() => {
+		getMoviesHandler();
+	}, [getMoviesHandler]);
+
+	let content = <p>There are no movies!</p>;
+	if (movies.length > 0) {
+		content = <MoviesList movies={movies} />;
+	}
+	if (error) {
+		content = <p>{error}</p>;
+	}
+	if (isLoading) {
+		content = <p>We are loading, please wait!</p>;
+	}
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={getMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>{content}</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
