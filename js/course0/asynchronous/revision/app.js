6c0fa84fb634daba544992c6747db2ad27735da6
Jose Boix <bmm00000@icloud.com>
2022-07-21 00:37:25 +0100

    revise promisifying apis

--- js/course0/asynchronous/revision/app.js
+++ js/course0/asynchronous/revision/app.js
@@ -1,7 +1,53 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// 	.then((response) => {
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
+
+// const ourPromise = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve('it worked!');
+// 	}, 2000);
+// });
+
+// ourPromise
+// 	.then((response) => {
+// 		console.log(response);
+// 		return 123;
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 		return new Promise((resolve, reject) => {
+// 			setTimeout(() => {
+// 				resolve('another custom promise resolved');
+// 			}, 2000);
+// 		});
+// 	})
+// 	.then((response) => {
+// 		console.log(response);
+// 	});
+
+//
+
+const ourTimer = (duration) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('ourTimer resolved');
+		}, duration);
+	});
+};
+
+ourTimer(2000)
 	.then((response) => {
-		return response.json();
+		console.log(response);
+		return 123;
 	})
 	.then((data) => {
 		console.log(data);
+		return ourTimer(2000);
+	})
+	.then((response) => {
+		console.log(response);
 	});
