489963ba452c1b1c656d9e92213678c30fbe22b2
Jose Boix <bmm00000@icloud.com>
2022-07-22 17:27:29 +0100

    practice promises

--- js/course0/asynchronous/revision/app.js
+++ js/course0/asynchronous/revision/app.js
@@ -31,23 +31,64 @@
 
 //
 
-const ourTimer = (duration) => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve('ourTimer resolved');
-		}, duration);
-	});
+// const ourTimer = (duration) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			reject('ourTimer rejected');
+// 		}, duration);
+// 	});
+// };
+
+// ourTimer(2000)
+// 	.then((response) => {
+// 		console.log(response);
+// 		return 123;
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 		return ourTimer(2000);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 		return 'haha';
+// 	})
+// 	.then((response) => {
+// 		console.log(response);
+// 	});
+
+//
+
+// fetch('https://jsonplacehoasdasdder.typicode.com/todos/1l;ajsdlfjasdlkf')
+// 	.then((response) => {
+// 		return response.json();
+// 	})
+// 	.catch((err) => {
+// 		console.log('only error block');
+// 		return 123;
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// 	.then((response) => {
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
+
+const sendRequest = async () => {
+	try {
+		const response = await fetch(
+			'https://jsonplaclkasjdfkl;dajskeholder.typicode.com/todos/1'
+		);
+		const data = await response.json();
+		console.log(data);
+	} catch (err) {
+		console.log('haha');
+	}
 };
 
-ourTimer(2000)
-	.then((response) => {
-		console.log(response);
-		return 123;
-	})
-	.then((data) => {
-		console.log(data);
-		return ourTimer(2000);
-	})
-	.then((response) => {
-		console.log(response);
-	});
+sendRequest();
+console.log('first thing');
