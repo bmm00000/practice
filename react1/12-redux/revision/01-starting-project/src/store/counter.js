5b1a9adfa632f56665a84c74e6a3bf5307716afc
Jose Boix <bmm00000@icloud.com>
2022-07-26 12:23:59 +0100

    organize files of redux toolkit demo

--- react1/12-redux/revision/01-starting-project/src/store/counter.js
+++ react1/12-redux/revision/01-starting-project/src/store/counter.js
@@ -1,4 +1,4 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialCounterState = { counter: 0, showCounter: true };
 
@@ -21,10 +21,6 @@ const counterSlice = createSlice({
 	},
 });
 
-const store = configureStore({
-	reducer: counterSlice.reducer,
-});
-
 export const counterActions = counterSlice.actions;
 
-export default store;
+export default counterSlice.reducer;
