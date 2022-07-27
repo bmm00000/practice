4981a68c50e656b2e95a0ace24a594e9b5f7a56d
Jose Boix <bmm00000@icloud.com>
2022-07-27 13:12:24 +0100

    revise mocks, spies

--- testing-js/revision/spies-mocks/01-starting-project/src/util/io.test.js
+++ testing-js/revision/spies-mocks/01-starting-project/src/util/io.test.js
@@ -1,10 +1,33 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
+import { promises as fs } from 'fs';
 
 import writeData from './io';
+
+vi.mock('fs'); // it replaces the whole 'fs' module with your implementation in the __mocks__ folder, or if you don't have it, then with spy empty functions.
+vi.mock('path', () => {
+	return {
+		default: {
+			join: (...args) => {
+				return args[args.length - 1];
+			},
+		},
+	};
+});
 
 it('should execute the writeFile method', () => {
 	const testData = 'Test';
 	const testFileName = 'test.txt';
+
+	writeData(testData, testFileName);
+
+	expect(fs.writeFile).toBeCalledWith(testFileName, testData);
+});
+
+it('should return a promise that resolves', () => {
+	const testData = 'Test';
+	const testFileName = 'test.txt';
+
+	writeData(testData, testFileName);
 
 	return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
 });
