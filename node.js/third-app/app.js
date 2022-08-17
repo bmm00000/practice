84ba3a44e0f909ed2b3e0e4cd0b511b6fd9f5aee
Jose Boix <bmm00000@icloud.com>
2022-08-18 00:01:36 +0100

    add ejs

--- node.js/third-app/app.js
+++ node.js/third-app/app.js
@@ -3,10 +3,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,7 +18,7 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	res.status(404).render('404', { pageTitle: 'Page Not Found!' });
 });
 
 app.listen(3000);
