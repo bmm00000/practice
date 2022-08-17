037bac95990dbf3fad85a94c551addfb682756b0
Jose Boix <bmm00000@icloud.com>
2022-08-17 18:13:29 +0100

    add assignment-3 to node

--- node.js/assignment-3/app.js
+++ node.js/assignment-3/app.js
@@ -3,18 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.router);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(usersRoutes);
 
-app.use((req, res) => {
+app.get((req, res) => {
 	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
