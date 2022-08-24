92e4671157246dd189178352c9ef8f5cdd3a7f42
Jose Boix <bmm00000@icloud.com>
2022-08-24 16:42:07 +0100

    add product detail to shop node.js app

---
+++ node.js/shop-app/controllers/shop.js
@@ -0,0 +1,56 @@
+const Product = require('../models/product');
+
+exports.getIndex = (req, res, next) => {
+	Product.fetchAll((products) => {
+		res.render('shop/index', {
+			prods: products,
+			pageTitle: 'Shop',
+			path: '/',
+		});
+	});
+};
+
+exports.getProducts = (req, res, next) => {
+	Product.fetchAll((products) => {
+		res.render('shop/product-list', {
+			prods: products,
+			pageTitle: 'All Products',
+			path: '/products',
+		});
+	});
+};
+
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId;
+	Product.findById(prodId, (product) => {
+		res.render('shop/product-detail', {
+			pageTitle: product.title,
+			path: '/products',
+			product: product,
+		});
+	});
+};
+
+exports.getCart = (req, res, next) => {
+	res.render('shop/cart', {
+		pageTitle: 'Your Cart',
+		path: '/cart',
+	});
+};
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId;
+	console.log(prodId);
+	res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+	res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders' });
+};
+
+exports.getCheckout = (req, res, next) => {
+	res.render('shop/checkout', {
+		pageTitle: 'Checkout',
+		path: '/checkout',
+	});
+};
