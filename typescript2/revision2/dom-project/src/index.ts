8d748d7be1d6085e053acd195e86a1d826692676
Jose Boix <bmm00000@icloud.com>
2022-08-28 13:11:48 +0100

    revise dom project in ts

--- typescript2/revision2/dom-project/src/index.ts
+++ typescript2/revision2/dom-project/src/index.ts
@@ -1,11 +1,43 @@
-function printTwice(message: string): void {
-	console.log(message);
-	console.log(message);
+interface Todo {
+	title: string;
+	completed: boolean;
 }
 
-// printTwice('hello there!');
-
 const button = document.getElementById('btn')!;
-button.addEventListener('click', function () {
-	alert('it works!');
-});
+const input = document.getElementById('todo') as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.querySelector('ul')!;
+
+const readTodos = (): Todo[] => {
+	const todosJSON = localStorage.getItem('todos');
+	if (!todosJSON) return [];
+	return JSON.parse(todosJSON);
+};
+
+const createTodoElement = (todo: Todo): void => {
+	const li = document.createElement('li');
+	li.innerText = todo.title;
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.checked = todo.completed;
+	checkbox.addEventListener('change', () => {
+		todo.completed = checkbox.checked;
+		localStorage.setItem('todos', JSON.stringify(todos));
+	});
+	li.append(checkbox);
+	list.appendChild(li);
+};
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodoElement);
+
+const submitHandler = (e: Event): void => {
+	e.preventDefault();
+	const todo: Todo = { title: input.value, completed: false };
+	todos.push(todo);
+	localStorage.setItem('todos', JSON.stringify(todos));
+	createTodoElement(todo);
+	input.value = '';
+};
+
+form.addEventListener('submit', submitHandler);
