b6b8f7f733ec51e916bcabbceff005b99a1041d8
Jose Boix <bmm00000@icloud.com>
2022-09-12 15:17:50 +0100

    add sessions

--- node.js/shop-app-2/controllers/shop.js
+++ node.js/shop-app-2/controllers/shop.js
@@ -8,6 +8,7 @@ exports.getIndex = (req, res, next) => {
 				prods: products,
 				pageTitle: 'Shop',
 				path: '/',
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => {
@@ -22,6 +23,7 @@ exports.getProducts = (req, res, next) => {
 				prods: products,
 				pageTitle: 'All Products',
 				path: '/products',
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => {
@@ -37,6 +39,7 @@ exports.getProduct = (req, res, next) => {
 				pageTitle: product.title,
 				path: '/products',
 				product: product,
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => {
@@ -53,6 +56,7 @@ exports.getCart = (req, res, next) => {
 				pageTitle: 'Your Cart',
 				path: '/cart',
 				products: products,
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -112,6 +116,7 @@ exports.getOrders = (req, res, next) => {
 				pageTitle: 'Your Orders',
 				path: '/orders',
 				orders: orders,
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
