9ab563151bd64d03de3070be8bb76b90599ce672
Jose Boix <bmm00000@icloud.com>
2022-09-14 22:46:03 +0100

    practice webpack with ts

---
+++ typescript2/revision2/webpack/src/utils.ts
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+export function add(x: number, y: number): number {
+	return _.add(x, y);
+}
+
+export function multiply(x: number, y: number): number {
+	return x * y;
+}
