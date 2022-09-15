5fabe64f38b9b7657922c1d0d9f7b54ee76d6340
Jose Boix <bmm00000@icloud.com>
2022-09-15 21:13:52 +0100

    add functions types in ts

--- typescript1/practice/src/index.ts
+++ typescript1/practice/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello there');
+function sum(x: number, y: number): number {
+	return x + y;
+}
+
+function greet(): void {
+	console.log('hello');
+}
+
+// let mySum: Function;
+let mySum: (a: number, b: number) => number;
+mySum = sum;
+
+// console.log(mySum(2, 2));
+
+function addAndHandle(x: number, y: number, cb: (num: number) => void): void {
+	const result = x + y;
+	cb(result);
+}
+
+addAndHandle(2, 2, (res) => {
+	console.log(res);
+});
