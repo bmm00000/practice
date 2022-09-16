2ade6398c5fb69529090f2106ee80b12582c5da2
Jose Boix <bmm00000@icloud.com>
2022-09-16 21:38:13 +0100

    practice ts classes

--- typescript1/practice/src/index.ts
+++ typescript1/practice/src/index.ts
@@ -1,53 +1,50 @@
-function sum(x: number, y: number): number {
-	return x + y;
-}
+class Department {
+	private employees: string[] = [];
+	constructor(private readonly id: string, private name: string) {}
 
-function greet(): void {
-	console.log('hello');
-}
-
-// let mySum: Function;
-let mySum: (a: number, b: number) => number;
-mySum = sum;
-
-// console.log(mySum(2, 2));
-
-//
-
-function addAndHandle(x: number, y: number, cb: (num: number) => void): void {
-	const result = x + y;
-	cb(result);
-}
-
-// addAndHandle(2, 2, (res) => {
-// 	console.log(res);
-// });
-
-let userInput: unknown;
-let userName: string;
-
-userInput = 'hello';
-userInput = 33;
-userInput = 'myname';
-
-if (typeof userInput === 'string') {
-	userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-	while (true) {
-		console.log('Im always here');
+	describe(this: Department): string {
+		return `This is the ${this.name} department with id: ${this.id}.`;
 	}
-	// throw { message, errorCode: code };
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
 
-// const canYouPrintThis = generateError('haha', 10);
-// console.log(canYouPrintThis);
+class ITDepartment extends Department {
+	public admins: string[];
+	constructor(id: string, admins: string[]) {
+		super(id, 'IT');
+		this.admins = admins;
+	}
 
-const button = document.querySelector('button')!;
+	printAdmins() {
+		console.log(this.admins);
+	}
+}
 
-const clickHandler = (message: string): void => {
-	console.log(message);
-};
+const itDepartment1 = new ITDepartment('a2', ['Dani', 'Jose']);
 
-button.addEventListener('click', clickHandler.bind(null, 'hello there!'));
+itDepartment1.addEmployee('Helen');
+
+console.log(itDepartment1);
+itDepartment1.printAdmins();
+
+// const accounting = new Department('a1', 'Accounting');
+// console.log(accounting.describe());
+
+// const accountingCopy = {
+// 	name: 'AccountingCopy',
+// 	describe: accounting.describe,
+// };
+// console.log(accountingCopy.describe());
+
+// accounting.addEmployee('Pedro');
+// accounting.addEmployee('Maxi');
+
+// accounting.printEmployeeInformation();
