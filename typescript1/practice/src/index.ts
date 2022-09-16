d50d7e80ac4574ff4642308b18bc77beb05e40ca
Jose Boix <bmm00000@icloud.com>
2022-09-16 17:06:26 +0100

    practice ts with nextgen js features

--- typescript1/practice/src/index.ts
+++ typescript1/practice/src/index.ts
@@ -12,11 +12,42 @@ mySum = sum;
 
 // console.log(mySum(2, 2));
 
+//
+
 function addAndHandle(x: number, y: number, cb: (num: number) => void): void {
 	const result = x + y;
 	cb(result);
 }
 
-addAndHandle(2, 2, (res) => {
-	console.log(res);
-});
+// addAndHandle(2, 2, (res) => {
+// 	console.log(res);
+// });
+
+let userInput: unknown;
+let userName: string;
+
+userInput = 'hello';
+userInput = 33;
+userInput = 'myname';
+
+if (typeof userInput === 'string') {
+	userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+	while (true) {
+		console.log('Im always here');
+	}
+	// throw { message, errorCode: code };
+}
+
+// const canYouPrintThis = generateError('haha', 10);
+// console.log(canYouPrintThis);
+
+const button = document.querySelector('button')!;
+
+const clickHandler = (message: string): void => {
+	console.log(message);
+};
+
+button.addEventListener('click', clickHandler.bind(null, 'hello there!'));
