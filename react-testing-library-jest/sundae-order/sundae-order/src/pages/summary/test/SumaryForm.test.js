5a62957705a289ac1278501627a0ddea692d88df
Jose Boix <bmm00000@icloud.com>
2022-10-31 21:08:54 +0000

    test and code 'popover responds to hover'

--- react-testing-library-jest/sundae-order/sundae-order/src/pages/summary/test/SumaryForm.test.js
+++ react-testing-library-jest/sundae-order/sundae-order/src/pages/summary/test/SumaryForm.test.js
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 it('button should be disabled if the ckeckbox is unchecked, and abled when checkbox is checked', async () => {
+	const user = userEvent.setup();
 	render(<SummaryForm />);
 
 	const checkboxEl = screen.getByRole('checkbox');
@@ -12,13 +13,32 @@ it('button should be disabled if the ckeckbox is unchecked, and abled when check
 	expect(checkboxEl).not.toBeChecked();
 	expect(buttonEl).toBeDisabled();
 
-	await userEvent.click(checkboxEl);
+	await user.click(checkboxEl);
 
 	expect(checkboxEl).toBeChecked();
 	expect(buttonEl).toBeEnabled();
 
-	await userEvent.click(checkboxEl);
+	await user.click(checkboxEl);
 
 	expect(checkboxEl).not.toBeChecked();
 	expect(buttonEl).toBeDisabled();
+});
+
+it('popover responds to hover', async () => {
+	const user = userEvent.setup();
+	render(<SummaryForm />);
+
+	const nullPopover = screen.queryByText(
+		/no ice cream will actually be delivered/i
+	);
+
+	expect(nullPopover).not.toBeInTheDocument();
+
+	const termsAndConditions = screen.getByText(/terms and conditions/i);
+	await user.hover(termsAndConditions);
+	const popover = screen.getByText(/no ice cream will actually be delivered/i);
+	expect(popover).toBeInTheDocument();
+
+	await user.unhover(termsAndConditions);
+	expect(popover).not.toBeInTheDocument();
 });
