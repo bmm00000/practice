16e2d5923efc92781d2d8e94bbd4a948ce3f630b
Jose Boix <bmm00000@icloud.com>
2022-11-12 21:11:39 +0000

    refactor button style

--- react-testing-library-jest/color-button1/color-button-app/src/components/Button.js
+++ react-testing-library-jest/color-button1/color-button-app/src/components/Button.js
@@ -9,14 +9,11 @@ const Button = ({ checked }) => {
 
 	const buttonText = clicked ? 'changed to blue!' : 'change to blue';
 	let buttonColor = clicked ? 'blue' : 'red';
-	if (checked) {
-		buttonColor = 'grey';
-	}
 
 	return (
 		<>
 			<button
-				style={{ color: buttonColor }}
+				style={{ color: checked ? 'grey' : buttonColor }}
 				onClick={buttonClickHandler}
 				disabled={checked}
 			>
