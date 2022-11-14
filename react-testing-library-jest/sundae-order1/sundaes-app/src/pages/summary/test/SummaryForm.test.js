445f0e5d50faebcee408698aa78e515fe9e718e0
Jose Boix <bmm00000@icloud.com>
2022-11-14 12:38:36 +0000

    refactor tests with userEvent@14

--- react-testing-library-jest/sundae-order1/sundaes-app/src/pages/summary/test/SummaryForm.test.js
+++ react-testing-library-jest/sundae-order1/sundaes-app/src/pages/summary/test/SummaryForm.test.js
@@ -14,27 +14,29 @@ test('checkbox is initially unchecked and button is disabled', () => {
 	expect(buttonEl).toBeDisabled();
 });
 
-test('when checkbox is checked, button is enabled', () => {
+test('when checkbox is checked, button is enabled', async () => {
+	const user = userEvent.setup();
 	render(<SummaryForm />);
 	const checkboxEl = screen.getByRole('checkbox', {
 		name: /terms and conditions/i,
 	});
 	const buttonEl = screen.getByRole('button', { name: /confirm order/i });
 
-	userEvent.click(checkboxEl);
+	await user.click(checkboxEl);
 
 	expect(buttonEl).toBeEnabled();
 });
 
-test('when checkbox is unchecked, button is disabled', () => {
+test('when checkbox is unchecked, button is disabled', async () => {
+	const user = userEvent.setup();
 	render(<SummaryForm />);
 	const checkboxEl = screen.getByRole('checkbox', {
 		name: /terms and conditions/i,
 	});
 	const buttonEl = screen.getByRole('button', { name: /confirm order/i });
 
-	userEvent.click(checkboxEl);
-	userEvent.click(checkboxEl);
+	await user.click(checkboxEl);
+	await user.click(checkboxEl);
 
 	expect(buttonEl).toBeDisabled();
 });
