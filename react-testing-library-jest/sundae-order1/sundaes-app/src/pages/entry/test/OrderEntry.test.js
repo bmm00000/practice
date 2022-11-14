a139c23c536978f9d87c436480d36c1b191115e7
Jose Boix <bmm00000@icloud.com>
2022-11-14 18:01:52 +0000

    fix bug in OrderEntry

--- react-testing-library-jest/sundae-order1/sundaes-app/src/pages/entry/test/OrderEntry.test.js
+++ react-testing-library-jest/sundae-order1/sundaes-app/src/pages/entry/test/OrderEntry.test.js
@@ -7,17 +7,20 @@ import OrderEntry from '../OrderEntry';
 test('displays alert message when request is not successful', async () => {
 	server.resetHandlers(
 		rest.get('https://localhost:3000/scoops', (req, res, ctx) => {
-			res(ctx.status(500));
+			return res(ctx.status(500));
 		}),
 		rest.get('https://localhost:3000/toppings', (req, res, ctx) => {
-			res(ctx.status(500));
+			return res(ctx.status(500));
 		})
 	);
 
 	render(<OrderEntry />);
 
 	await waitFor(async () => {
-		const alerts = await screen.findAllByRole('alert');
+		const alerts = await screen.findAllByRole(
+			'alert',
+			'displays alert message when request is not successful'
+		);
 		expect(alerts).toHaveLength(2);
 	});
 });
