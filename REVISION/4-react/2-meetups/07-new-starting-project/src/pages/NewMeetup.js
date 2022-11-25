1c3556b48c803bdd92e4963ff214b3593a956fbf
Jose Boix <bmm00000@icloud.com>
2022-11-25 19:12:10 +0000

    add fetch all meetups when app loads

--- REVISION/4-react/2-meetups/07-new-starting-project/src/pages/NewMeetup.js
+++ REVISION/4-react/2-meetups/07-new-starting-project/src/pages/NewMeetup.js
@@ -1,5 +1,36 @@
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 const NewMeetupPage = (props) => {
-	return <>New Meetup Page</>;
+	const history = useHistory();
+
+	const submitMeetupHandler = (meetup) => {
+		fetch(
+			'https://meetups-app-42f83-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetup),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				history.replace('/');
+			});
+	};
+
+	return (
+		<section>
+			<h1>Submit your meetup</h1>
+			<NewMeetupForm onFormSubmit={submitMeetupHandler} />
+		</section>
+	);
 };
 
 export default NewMeetupPage;
