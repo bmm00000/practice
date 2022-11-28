e92a05a9a57e4dfebe2e2a9505b5aa278daf3c9d
Jose Boix <bmm00000@icloud.com>
2022-11-28 16:51:11 +0000

    add context to meetups react app

--- REVISION/4-react/2-meetups/07-new-starting-project/src/components/meetups/Meetup.js
+++ REVISION/4-react/2-meetups/07-new-starting-project/src/components/meetups/Meetup.js
@@ -1,8 +1,29 @@
+import { useContext } from 'react';
+
+import FavoritesContext from '../../store/favorites-context';
 import Card from '../ui/Card';
 
 import classes from './Meetup.module.css';
 
 const Meetup = (props) => {
+	const favoritesCtx = useContext(FavoritesContext);
+
+	const isFave = favoritesCtx.isFavorite(props.id);
+
+	const buttonText = isFave ? 'Remove from Favorites' : 'Add to Favorites';
+
+	const favoritesToggleHandler = () => {
+		isFave
+			? favoritesCtx.removeFavorite(props.id)
+			: favoritesCtx.addFavorite({
+					id: props.id,
+					title: props.title,
+					image: props.image,
+					address: props.address,
+					description: props.description,
+			  });
+	};
+
 	return (
 		<li className={classes.item}>
 			<Card>
@@ -15,7 +36,7 @@ const Meetup = (props) => {
 					<p>{props.description}</p>
 				</div>
 				<div className={classes.actions}>
-					<button>To Favorites</button>
+					<button onClick={favoritesToggleHandler}>{buttonText}</button>
 				</div>
 			</Card>
 		</li>
