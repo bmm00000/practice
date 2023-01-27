f54ebbd708cc80228c818b3ae5a2e3b69e27e18e
Jose Boix <bmm00000@icloud.com>
2023-01-27 17:03:27 +0000

    make the matcher of RepositoriesSummary.test.js more flexible with RegExp()

--- REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
+++ REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('should render the language of the repo', () => {
+test('should render the data passed to the RepositoriesSummary', () => {
 	const repository = {
 		stargazers_count: 1,
-		open_issues: 1,
-		forks: 1,
+		open_issues: 2,
+		forks: 3,
 		language: 'test-language',
 	};
 	render(<RepositoriesSummary repository={repository} />);
 
-	const languageEl = screen.getByText('test-language');
-	expect(languageEl).toBeInTheDocument();
+	for (let key in repository) {
+		const value = repository[key];
+		const element = screen.getByText(new RegExp(value));
+		expect(element).toBeInTheDocument();
+	}
 });
