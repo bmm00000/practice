10acc9ffae8afe93dd2494448ddac344d3fa5502
Jose Boix <bmm00000@icloud.com>
2023-01-27 19:49:58 +0000

    test'shows a file icon with the appropriate icon'

--- REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
+++ REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
@@ -5,7 +5,7 @@ import RepositoriesListItem from './RepositoriesListItem';
 const renderComponent = () => {
 	const repository = {
 		full_name: 'repo-full-name',
-		language: 'repo-language',
+		language: 'Javascript',
 		description: 'repo-description',
 		owner: 'repo-owner',
 		name: 'repo-name',
@@ -23,8 +23,15 @@ const renderComponent = () => {
 test('should render the link of the github repo', async () => {
 	const { repository } = renderComponent();
 
-	await screen.findByRole('img', { name: /repo-language/i });
+	await screen.findByRole('img', { name: 'Javascript' });
 
 	const link = screen.getByRole('link', { name: 'github repo link' });
 	expect(link).toHaveAttribute('href', repository.html_url);
+});
+
+test('shows a file icon with the appropriate icon', async () => {
+	renderComponent();
+
+	const icon = await screen.findByRole('img', { name: 'Javascript' });
+	expect(icon).toHaveClass('js-icon');
 });
