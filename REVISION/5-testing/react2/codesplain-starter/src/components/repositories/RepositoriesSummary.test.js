c5df50de347fb9e1398e9ff42af5ae89fdd8227c
Jose Boix <bmm00000@icloud.com>
2023-01-27 16:49:25 +0000

    fix absence of language in repos

--- REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
+++ REVISION/5-testing/react2/codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -1,4 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('should render the language of the repo', () => {});
+test('should render the language of the repo', () => {
+	const repository = {
+		stargazers_count: 1,
+		open_issues: 1,
+		forks: 1,
+		language: 'test-language',
+	};
+	render(<RepositoriesSummary repository={repository} />);
+
+	const languageEl = screen.getByText('test-language');
+	expect(languageEl).toBeInTheDocument();
+});
