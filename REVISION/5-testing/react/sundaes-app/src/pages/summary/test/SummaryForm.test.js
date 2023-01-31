c14671f28e5dffb0b4cef215177ca39788e741f6
Jose Boix <bmm00000@icloud.com>
2023-01-31 20:00:03 +0000

    test and code popover

--- REVISION/5-testing/react/sundaes-app/src/pages/summary/test/SummaryForm.test.js
+++ REVISION/5-testing/react/sundaes-app/src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 test('initially, checkbox is unchecked, button disabled', () => {
@@ -14,7 +14,8 @@ test('initially, checkbox is unchecked, button disabled', () => {
 	expect(buttonElement).toBeDisabled();
 });
 
-test('when checkbox gets checked, button gets enabled; and when checkbox gets unchecked, button gets disabled', () => {
+test('when checkbox gets checked, button gets enabled; and when checkbox gets unchecked, button gets disabled', async () => {
+	const user = userEvent.setup();
 	render(<SummaryForm />);
 
 	const checkboxElement = screen.getByRole('checkbox', {
@@ -22,11 +23,38 @@ test('when checkbox gets checked, button gets enabled; and when checkbox gets un
 	});
 	const buttonElement = screen.getByRole('button', { name: /confirm order/i });
 
-	user.click(checkboxElement);
+	await user.click(checkboxElement);
 
 	expect(buttonElement).toBeEnabled();
 
-	user.click(checkboxElement);
+	await user.click(checkboxElement);
 
 	expect(buttonElement).toBeDisabled();
+});
+
+test('popover responds to hover', async () => {
+	const user = userEvent.setup();
+	render(<SummaryForm />);
+
+	const nullPopover = screen.queryByText(
+		/no ice cream will actually be delivered/i
+	);
+
+	expect(nullPopover).not.toBeInTheDocument();
+
+	const termsAndConditions = screen.getByText(/terms and conditions/i);
+	await user.hover(termsAndConditions);
+
+	const popoverElement = screen.getByText(
+		/no ice cream will actually be delivered/i
+	);
+	expect(popoverElement).toBeInTheDocument();
+
+	await user.unhover(termsAndConditions);
+
+	const secondNullPopover = screen.queryByText(
+		/no ice cream will actually be delivered/i
+	);
+
+	expect(secondNullPopover).not.toBeInTheDocument();
 });
