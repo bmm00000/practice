2b4e056a298311d1ad11ef117a1180f20a13295b
Jose Boix <bmm00000@icloud.com>
2023-02-01 19:32:17 +0000

    test and code 'should render the images of the toppings'

--- REVISION/5-testing/react/sundaes-app/src/entry/test/Options.test.js
+++ REVISION/5-testing/react/sundaes-app/src/entry/test/Options.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { findAllByRole, render, screen } from '@testing-library/react';
 import Options from '../Options';
 
 test('should render the images of the scoops', async () => {
@@ -13,5 +13,24 @@ test('should render the images of the scoops', async () => {
 		'Vanilla scoop',
 		'Chocolate scoop',
 		'Salted caramel scoop',
+	]);
+});
+
+test('should render the images of the toppings', async () => {
+	render(<Options optionType='topping' />);
+
+	const toppingImages = await screen.findAllByRole('img', {
+		name: /topping$/i,
+	});
+	expect(toppingImages).toHaveLength(6);
+
+	const toppingImagesAlt = toppingImages.map((image) => image.alt);
+	expect(toppingImagesAlt).toEqual([
+		'M&Ms topping',
+		'Hot fudge topping',
+		'Peanut butter cups topping',
+		'Gummi bears topping',
+		'Mochi topping',
+		'Cherries topping',
 	]);
 });
