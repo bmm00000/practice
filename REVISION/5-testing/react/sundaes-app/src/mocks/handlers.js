d2fd851a4af0b65a6824ae7a909494c96c5390d0
Jose Boix <bmm00000@icloud.com>
2023-02-01 17:54:05 +0000

    add msw setup

---
+++ REVISION/5-testing/react/sundaes-app/src/mocks/handlers.js
@@ -0,0 +1,57 @@
+import { rest } from 'msw';
+
+export const handlers = [
+	rest.get('/scoops', (req, res, ctx) => {
+		return res(
+			ctx.json([
+				{
+					name: 'Mint chip',
+					imagePath: '/images/mint-chip.png',
+				},
+				{
+					name: 'Vanilla',
+					imagePath: '/images/vanilla.png',
+				},
+				{
+					name: 'Chocolate',
+					imagePath: '/images/chocolate.png',
+				},
+				{
+					name: 'Salted caramel',
+					imagePath: '/images/salted-caramel.png',
+				},
+			])
+		);
+	}),
+
+	rest.get('/toppings', (req, res, ctx) => {
+		return res(
+			ctx.json([
+				{
+					name: 'M&Ms',
+					imagePath: '/images/m-and-ms.png',
+				},
+				{
+					name: 'Hot fudge',
+					imagePath: '/images/hot-fudge.png',
+				},
+				{
+					name: 'Peanut butter cups',
+					imagePath: '/images/peanut-butter-cups.png',
+				},
+				{
+					name: 'Gummi bears',
+					imagePath: '/images/gummi-bears.png',
+				},
+				{
+					name: 'Mochi',
+					imagePath: '/images/mochi.png',
+				},
+				{
+					name: 'Cherries',
+					imagePath: '/images/cherries.png',
+				},
+			])
+		);
+	}),
+];
