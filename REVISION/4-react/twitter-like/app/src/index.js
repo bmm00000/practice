d71e606ab93a6c6b3438c65f7514d788f2894086
Jose Boix <bmm00000@icloud.com>
2023-02-05 20:32:26 +0000

    add routes folder and RootLayout component to twitter-like ap

--- REVISION/4-react/twitter-like/app/src/index.js
+++ REVISION/4-react/twitter-like/app/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './routes/RootLayout';
 import App from './App';
 import NewPost from './components/NewPost';
 
 const router = createBrowserRouter([
-	{ path: '/', element: <App /> },
-	{ path: '/new-post', element: <NewPost /> },
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <App /> },
+			{ path: '/new-post', element: <NewPost /> },
+		],
+	},
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
