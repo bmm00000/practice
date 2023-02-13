36345e9d3292a7853b4fb85f60ab9d6f678fecc5
Jose Boix <bmm00000@icloud.com>
2023-02-13 19:21:59 +0000

    fix io.test.js

--- REVISION/testing/js/2-mocks-spies/01-starting-project/src/util/io.test.js
+++ REVISION/testing/js/2-mocks-spies/01-starting-project/src/util/io.test.js
@@ -2,18 +2,18 @@ import { it, expect, vi } from 'vitest';
 vi.mock('fs', () => {
 	return {
 		promises: {
-			writeFile: (storagePath, data) => {
+			writeFile: vi.fn((storagePath, data) => {
 				return new Promise((resolve, reject) => resolve());
-			},
+			}),
 		},
 	};
 });
 vi.mock('path', () => {
 	return {
 		default: {
-			join: (...args) => {
+			join: vi.fn((...args) => {
 				return args[args.length - 1];
-			},
+			}),
 		},
 	};
 });
@@ -21,21 +21,21 @@ import writeData from './io';
 import { promises as fs } from 'fs';
 import path from 'path';
 
-it('should call the writeFile method from the fs module', () => {
+it('should resolve the promise from the writeFile method in the fs module', () => {
 	const data = 'data';
 	const fileName = 'file-name.txt';
 
 	return expect(writeData(data, fileName)).resolves.toBeUndefined();
 });
 
-// it('should call the writeFile method from the fs module', () => {
-// 	const data = 'data';
-// 	const fileName = 'file-name.txt';
+it('should call the writeFile method from the fs module', () => {
+	const data = 'data';
+	const fileName = 'file-name.txt';
 
-// 	writeData(data, fileName);
+	writeData(data, fileName);
 
-// 	expect(fs.writeFile).toHaveBeenCalled();
-// });
+	expect(fs.writeFile).toHaveBeenCalled();
+});
 
 it('should call the writeFile method from the fs module with fileName and data as arguments', () => {
 	const data = 'data';
@@ -43,23 +43,14 @@ it('should call the writeFile method from the fs module with fileName and data a
 
 	writeData(data, fileName);
 
-	expect(fs.writeFile).toBeCalledWith(fileName, data);
+	expect(fs.writeFile).toHaveBeenCalledWith(fileName, data);
 });
 
-// it('should call the join method from the path module', () => {
-// 	const data = 'data';
-// 	const fileName = 'file-name.txt';
+it('should call the join method from the path module', () => {
+	const data = 'data';
+	const fileName = 'file-name.txt';
 
-// 	writeData(data, fileName);
+	writeData(data, fileName);
 
-// 	expect(path.join).toHaveBeenCalled();
-// });
-
-// it('should call the join method from the path module with fileName as an argument', () => {
-// 	const data = 'data';
-// 	const fileName = 'file-name.txt';
-
-// 	writeData(data, fileName);
-
-// 	expect(path.join).toHaveBeenCalledWith(fileName);
-// });
+	expect(path.join).toHaveBeenCalled();
+});
