9af0a516e863998ebf1e9639e6328ff34138fe9c
Jose Boix <bmm00000@icloud.com>
2023-02-24 21:32:45 +0100

    test Async component

--- REVISION/react/0-testing/react-testing/src/components/Async.test.js
+++ REVISION/react/0-testing/react-testing/src/components/Async.test.js
@@ -1,3 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import Async from './Async';
 
-test('something', () => {});
+describe('Async component', () => {
+	test('should render list items upon successful fetch from api', async () => {
+		window.fetch = jest.fn().mockResolvedValueOnce({
+			json: async () => [{ id: '1', title: 'First post' }],
+		});
+		render(<Async />);
+
+		const listItemsArray = await screen.findAllByRole('listitem');
+		expect(listItemsArray).not.toHaveLength(0);
+	});
+});
